1774fd63d41f648f80a5cd64498885fd3767957e
RomjanHossain <romjanhossain726526@gmail.com>
2023-11-14 06:56:41 +0600

    first commit
    lets stage this

---
+++ android/app/src/main/kotlin/com/dstore/expense/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.dstore.expense.expense_tracker
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_staging.dart
@@ -0,0 +1,6 @@
+import 'package:expense_tracker/app/app.dart';
+import 'package:expense_tracker/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
