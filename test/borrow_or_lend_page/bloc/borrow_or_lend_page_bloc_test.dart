e9e4efb024cfabdc37bca7fe879739c597ca28a9
RomjanHossain <romjan@skiff.com>
2023-11-15 15:21:02 +0600

    test

---
+++ test/borrow_or_lend_page/bloc/borrow_or_lend_page_bloc_test.dart
@@ -0,0 +1,30 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:bloc_test/bloc_test.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:expense_tracker/presentation/pages/borrow_or_lend_page/bloc/bloc.dart';
+
+void main() {
+  group('BorrowOrLendPageBloc', () {
+    group('constructor', () {
+      test('can be instantiated', () {
+        expect(
+          BorrowOrLendPageBloc(),
+          isNotNull,
+        );
+      });
+    });
+
+    test('initial state has default value for customProperty', () {
+      final borrowOrLendPageBloc = BorrowOrLendPageBloc();
+      expect(borrowOrLendPageBloc.state.customProperty, equals('Default Value'));
+    });
+
+    blocTest<BorrowOrLendPageBloc, BorrowOrLendPageState>(
+      'CustomBorrowOrLendPageEvent emits nothing',
+      build: BorrowOrLendPageBloc.new,
+      act: (bloc) => bloc.add(const CustomBorrowOrLendPageEvent()),
+      expect: () => <BorrowOrLendPageState>[],
+    );
+  });
+}
