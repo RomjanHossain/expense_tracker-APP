dc3d49cb1b05abd42c62e4830f061c0f088df3bc
RomjanHossain <romjanhossain726526@gmail.com>
2023-11-15 15:13:15 +0600

    standred and dark theme

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -1,5 +1,6 @@
 import 'package:expense_tracker/app/ui/app_ui.dart';
 import 'package:expense_tracker/l10n/l10n.dart';
+import 'package:expense_tracker/presentation/pages/app_home_page/app_home_page.dart';
 import 'package:expense_tracker/presentation/pages/splash_screen/splash_screen.dart';
 import 'package:flutter/material.dart';
 
@@ -10,10 +11,12 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ExpenseTrackerTheme.standard,
-      darkTheme: ExpenseTrackerTheme.standard,
+      darkTheme: ExpenseTrackerTheme.darkTheme,
+      themeMode: ThemeMode.light,
       localizationsDelegates: AppLocalizations.localizationsDelegates,
       supportedLocales: AppLocalizations.supportedLocales,
-      home: const SplashScreenPage(),
+      // home: const SplashScreenPage(),
+      home: const AppHomePagePage(),
     );
   }
 }
