8b64cc0d512fefda54a367ad5d0f578b93995517
RomjanHossain <romjanhossain726526@gmail.com>
2023-11-15 15:21:02 +0600

    test

---
+++ test/borrow_or_lend_page/view/borrow_or_lend_page_page_test.dart
@@ -0,0 +1,20 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:expense_tracker/presentation/pages/borrow_or_lend_page/borrow_or_lend_page.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('BorrowOrLendPagePage', () {
+    group('route', () {
+      test('is routable', () {
+        expect(BorrowOrLendPagePage.route(), isA<MaterialPageRoute>());
+      });
+    });
+
+    testWidgets('renders BorrowOrLendPageView', (tester) async {
+      await tester.pumpWidget(MaterialApp(home: BorrowOrLendPagePage()));
+      expect(find.byType(BorrowOrLendPageView), findsOneWidget);
+    });
+  });
+}
