9d392985d078767d148e9933e32f9adcff03e0e7
RomjanHossain <romjan@skiff.com>
2023-11-15 09:28:50 +0600

    ignoring the splash screen test

---
+++ test/splash_screen/view/widgets/splash_screen_body_test.dart
@@ -0,0 +1,20 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:expense_tracker/presentation/pages/splash_screen/splash_screen.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('SplashScreenBody', () {
+    testWidgets('renders Text', (tester) async { 
+      await tester.pumpWidget(
+        BlocProvider(
+          create: (context) => SplashScreenBloc(),
+          child: MaterialApp(home: SplashScreenBody()),
+        ),
+      );
+
+      expect(find.byType(Text), findsOneWidget);
+    });
+  });
+}
