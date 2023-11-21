443da72a91a705d31552882526f22b5adc098884
RomjanHossain <romjan@skiff.com>
2023-11-21 16:56:52 +0600

    added two cubit to multibloc

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -1,5 +1,7 @@
 import 'package:expense_tracker/app/ui/app_ui.dart';
 import 'package:expense_tracker/l10n/l10n.dart';
+import 'package:expense_tracker/presentation/cubit/dropdown_data/dropdown_category_method_cubit.dart';
+import 'package:expense_tracker/presentation/cubit/dropdown_data/dropdown_expense_method_cubit.dart';
 import 'package:expense_tracker/presentation/cubit/expense_text_controller_cubit.dart';
 import 'package:expense_tracker/presentation/pages/app_home_page/app_home_page.dart';
 import 'package:flutter/material.dart';
@@ -14,6 +16,16 @@ class App extends StatelessWidget {
         // ExpenseTextControllerCubit
         BlocProvider<ExpenseTextControllerCubit>(
           create: (context) => ExpenseTextControllerCubit(),
+        ),
+
+        /// dropdown values [expense methods]
+        BlocProvider(
+          create: (context) => DropdownExpenseMethodCubit(),
+        ),
+
+        /// dropdown values [expended Category methods]
+        BlocProvider(
+          create: (context) => DropdownCategoryMethodCubit(),
         ),
       ],
       child: MaterialApp(
