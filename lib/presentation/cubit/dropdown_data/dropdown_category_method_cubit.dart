280195fad211a037c97b9b4d5e8850dd61dd7101
RomjanHossain <romjanhossain726526@gmail.com>
2023-11-21 16:56:59 +0600

    default to zero

--- lib/presentation/cubit/dropdown_data/dropdown_category_method_cubit.dart
+++ lib/presentation/cubit/dropdown_data/dropdown_category_method_cubit.dart
@@ -1,7 +1,7 @@
 import 'package:bloc/bloc.dart';
 
 class DropdownCategoryMethodCubit extends Cubit<int> {
-  DropdownCategoryMethodCubit() : super(0);
+  DropdownCategoryMethodCubit() : super(1);
 
   void changeIndex(int index) {
     emit(index-1);
