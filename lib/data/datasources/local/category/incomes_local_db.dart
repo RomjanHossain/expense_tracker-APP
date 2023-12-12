9d80adf145341a14f7d64cfa8cfb6904fb1990ad
RomjanHossain <romjan@skiff.com>
2023-12-12 07:50:18 +0600

    icomes local do to model

--- lib/data/datasources/local/category/incomes_local_db.dart
+++ lib/data/datasources/local/category/incomes_local_db.dart
@@ -18,3 +18,28 @@ const incomeLocalData = {
     'Scholarship': '🎓'
   }
 };
+
+/*
+Category(
+  id: 'incomeCategories',
+  title: 'Income Categories',
+  items: [
+    ExpenseItem(id: 'salary', title: 'Salary', icon: '💰'),
+    ExpenseItem(id: 'freelanceWork', title: 'Freelance Work', icon: '💻'),
+    ExpenseItem(id: 'sideGig', title: 'Side Gig', icon: '🚀'),
+    ExpenseItem(id: 'investmentDividends', title: 'Investment Dividends', icon: '📈'),
+    ExpenseItem(id: 'rentalIncome', title: 'Rental Income', icon: '🏠'),
+    ExpenseItem(id: 'businessProfits', title: 'Business Profits', icon: '📊'),
+    ExpenseItem(id: 'gifts', title: 'Gifts', icon: '🎁'),
+    ExpenseItem(id: 'other', title: 'Other', icon: '🌐'),
+    ExpenseItem(id: 'bonus', title: 'Bonus', icon: '🎉'),
+    ExpenseItem(id: 'commissions', title: 'Commissions', icon: '📈'),
+    ExpenseItem(id: 'tips', title: 'Tips', icon: '💡'),
+    ExpenseItem(id: 'royalties', title: 'Royalties', icon: '👑'),
+    ExpenseItem(id: 'refunds', title: 'Refunds', icon: '⏪'),
+    ExpenseItem(id: 'alimony', title: 'Alimony', icon: '💔'),
+    ExpenseItem(id: 'childSupport', title: 'Child Support', icon: '👶'),
+    ExpenseItem(id: 'scholarship', title: 'Scholarship', icon: '🎓'),
+  ],
+)
+*/
