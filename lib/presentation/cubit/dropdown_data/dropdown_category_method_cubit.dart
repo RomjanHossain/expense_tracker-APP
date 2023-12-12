6552e8983c97b07a7b2344687a52491708b66a41
RomjanHossain <romjanhossain726526@gmail.com>
2023-12-12 12:07:54 +0600

    changed widget name and others

--- lib/presentation/cubit/dropdown_data/dropdown_category_method_cubit.dart
+++
@@ -1,9 +0,0 @@
-import 'package:bloc/bloc.dart';
-
-class DropdownCategoryMethodCubit extends Cubit<int> {
-  DropdownCategoryMethodCubit() : super(1);
-
-  void changeIndex(int index) {
-    emit(index-1);
-  }
-}
