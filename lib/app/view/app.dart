56e1e056e7389b2ff7acdbd1974b71bbe7fce87e
RomjanHossain <romjan@skiff.com>
2023-12-12 12:07:06 +0600

    changed the name to income

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -1,7 +1,7 @@
 import 'package:expense_tracker/app/ui/app_ui.dart';
 import 'package:expense_tracker/l10n/l10n.dart';
-import 'package:expense_tracker/presentation/cubit/dropdown_data/dropdown_category_method_cubit.dart';
 import 'package:expense_tracker/presentation/cubit/dropdown_data/dropdown_expense_method_cubit.dart';
+import 'package:expense_tracker/presentation/cubit/dropdown_data/dropdown_income_method_cubit.dart';
 import 'package:expense_tracker/presentation/cubit/expense_text_controller_cubit.dart';
 import 'package:expense_tracker/presentation/pages/app_home_page/app_home_page.dart';
 import 'package:flutter/material.dart';
@@ -25,7 +25,7 @@ class App extends StatelessWidget {
 
         /// dropdown values [expended Category methods]
         BlocProvider(
-          create: (context) => DropdownCategoryMethodCubit(),
+          create: (context) => DropdownIncomeMethodCubit(),
         ),
       ],
       child: MaterialApp(
