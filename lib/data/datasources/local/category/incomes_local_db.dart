7e02c6dc65d10cfb1aac24c6155dcfdb1e6813b0
RomjanHossain <romjanhossain726526@gmail.com>
2023-12-12 07:06:14 +0600

    income local data

---
+++ lib/data/datasources/local/category/incomes_local_db.dart
@@ -0,0 +1,20 @@
+const incomeLocalData = {
+  'incomeCategories': {
+    'Salary': '💰',
+    'Freelance Work': '💻',
+    'Side Gig': '🚀',
+    'Investment Dividends': '📈',
+    'Rental Income': '🏠',
+    'Business Profits': '📊',
+    'Gifts': '🎁',
+    'Other': '🌐',
+    'Bonus': '🎉',
+    'Commissions': '📈',
+    'Tips': '💡',
+    'Royalties': '👑',
+    'Refunds': '⏪',
+    'Alimony': '💔',
+    'Child Support': '👶',
+    'Scholarship': '🎓'
+  }
+};
