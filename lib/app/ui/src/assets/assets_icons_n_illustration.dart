d3570ec75e3781eb65a524ca99921dc1fa673f64
RomjanHossain <romjan@skiff.com>
2023-12-23 22:45:25 +0600

    asset ui

---
+++ lib/app/ui/src/assets/assets_icons_n_illustration.dart
@@ -0,0 +1,109 @@
+abstract class ExpenseAssets {
+  static const String _icons = 'assets/icons/';
+  static const String _illustrations = 'assets/illustrations/';
+
+  /*
+    All icons are down below
+  */
+  // plus sign icon
+  static const String plusIcon = '${_icons}add.svg';
+  // arrow down
+  static const String arrowDownIcon = '${_icons}arrow_down.svg';
+  // arrow left long
+  static const String arrowLeftLongIcon = '${_icons}arrow_left_long.svg';
+  // arrow right
+  static const String arrowRightIcon = '${_icons}arrow_right.svg';
+  // attachment icon
+  static const String attachmentIcon = '${_icons}attachment.svg';
+  // camera
+  static const String cameraIcon = '${_icons}camera.svg';
+  // car
+  static const String carIcon = '${_icons}car.svg';
+  // close
+  static const String closeIcon = '${_icons}close.svg';
+  // create
+  static const String createIcon = '${_icons}create.svg';
+  // create_fill
+  static const String createFillIcon = '${_icons}create_fill.svg';
+  // currency_exchange
+  static const String currencyExchangeIcon = '${_icons}currency_exchange.svg';
+  // download
+  static const String downloadIcon = '${_icons}download.svg';
+  // edit
+  static const String editIcon = '${_icons}edit.svg';
+  // expense
+  static const String expenseIcon = '${_icons}expense.svg';
+  // flat_color_icons_google
+  static const String flatColorIconsGoogleIcon =
+      '${_icons}flat_color_icons_google.svg';
+  // gallery
+  static const String galleryIcon = '${_icons}gallery.svg';
+  // home
+  static const String homeIcon = '${_icons}home.svg';
+  // income
+  static const String incomeIcon = '${_icons}income.svg';
+  // line_chart
+  static const String lineChartIcon = '${_icons}line_chart.svg';
+  // logout
+  static const String logoutIcon = '${_icons}logout.svg';
+  // more_horizontal
+  static const String moreHorizontalIcon = '${_icons}more_horizontal.svg';
+  // new
+  static const String newIcon = '${_icons}new.svg';
+  // notifiaction
+  static const String notificationIcon = '${_icons}notification.svg';
+  // notification
+  static const String notificationFillIcon = '${_icons}notification_fill.svg';
+  // pie_chart
+  static const String pieChartIcon = '${_icons}pie_chart.svg';
+  // recurring_bill
+  static const String recurringBillIcon = '${_icons}recurring_bill.svg';
+  // restaurant
+  static const String restaurantIcon = '${_icons}restaurant.svg';
+  // salary
+  static const String salaryIcon = '${_icons}salary.svg';
+  // search
+  static const String searchIcon = '${_icons}search.svg';
+  // settings
+  static const String settingsIcon = '${_icons}settings.svg';
+  // shopping_bag
+  static const String shoppingBagIcon = '${_icons}shopping_bag.svg';
+  // show
+  static const String showIcon = '${_icons}show.svg';
+  // sort
+  static const String sortIcon = '${_icons}sort.svg';
+  // sort_highest_lowest
+  static const String sortHighestLowestIcon =
+      '${_icons}sort_highest_lowest.svg';
+  // success
+  static const String successIcon = '${_icons}success.svg';
+  // transaction
+  static const String transactionIcon = '${_icons}transaction.svg';
+  // transaction_color
+  static const String transactionColorIcon = '${_icons}transaction_color.svg';
+  // trash
+  static const String trashIcon = '${_icons}trash.svg';
+  // user
+  static const String userIcon = '${_icons}user.svg';
+  // wallet
+  static const String walletIcon = '${_icons}wallet.svg';
+  // warning
+  static const String warningIcon = '${_icons}warning.svg';
+
+  /*
+    All illustrations are down below
+  */
+  // Export Data
+  static const String exportDataIll = '${_illustrations}export_data.svg';
+  // Gain total control of your money
+  static const String gainTotalControlOfYourMoneyIll =
+      '${_illustrations}gain_total_control_of_your_money.svg';
+  // Know where your money goes
+  static const String knowWhereYourMoneyGoesIll =
+      '${_illustrations}know_where_your_money_goes.svg';
+  // Planning ahead
+  static const String planningAheadIll = '${_illustrations}planning_ahead.svg';
+  // Your email is on the way
+  static const String yourEmailIsOnTheWayIll =
+      '${_illustrations}your_email_is_on_the_way.svg';
+}
