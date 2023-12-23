1581711b1c6b91c81ff31f481ae3c80b31eb3c3e
RomjanHossain <romjan@skiff.com>
2023-12-23 22:45:51 +0600

    themeee

--- lib/app/ui/src/theme.dart
+++ lib/app/ui/src/theme.dart
@@ -1,27 +1,25 @@
 import 'package:expense_tracker/app/ui/app_ui.dart';
 import 'package:flutter/material.dart';
 
-const _smallTextScaleFactor = 0.80;
-const _largeTextScaleFactor = 1.20;
-
 /// Namespace for the ExpenseTracker [ThemeData].
 class ExpenseTrackerTheme {
   /// Standard `ThemeData` for ExpenseTracker UI.
   static ThemeData get standard {
     return ThemeData(
-      scaffoldBackgroundColor: ExpenseTrackerColors.whiteBackground,
-      colorScheme:
-          ColorScheme.fromSwatch(accentColor: ExpenseTrackerColors.primary),
-      appBarTheme: _appBarTheme,
+      scaffoldBackgroundColor: ExpenseTrackerColors.scaffoldBackground,
+      colorScheme: ColorScheme.fromSwatch(
+        accentColor: ExpenseTrackerColors.violet,
+      ),
+      // appBarTheme: _appBarTheme,
       elevatedButtonTheme: _elevatedButtonTheme,
       outlinedButtonTheme: _outlinedButtonTheme,
-      textTheme: _textTheme,
-      dialogBackgroundColor: ExpenseTrackerColors.whiteBackground,
-      dialogTheme: _dialogTheme,
-      tooltipTheme: _tooltipTheme,
-      bottomSheetTheme: _bottomSheetTheme,
-      tabBarTheme: _tabBarTheme,
-      dividerTheme: _dividerTheme,
+      // textTheme: _textTheme,
+      // // dialogBackgroundColor: ExpenseTrackerColors.whiteBackground,
+      // dialogTheme: _dialogTheme,
+      // tooltipTheme: _tooltipTheme,
+      // bottomSheetTheme: _bottomSheetTheme,
+      // tabBarTheme: _tabBarTheme,
+      // dividerTheme: _dividerTheme,
       useMaterial3: true,
     );
   }
@@ -29,162 +27,163 @@ class ExpenseTrackerTheme {
   /// DarkTheme `ThemeData` for ExpenseTracker UI.
   static ThemeData get darkTheme {
     return ThemeData(
-      scaffoldBackgroundColor: ExpenseTrackerColors.darkBackground,
+      // scaffoldBackgroundColor: ExpenseTrackerColors.darkBackground,
       useMaterial3: true,
       colorScheme: ColorScheme.fromSwatch(
-        accentColor: ExpenseTrackerColors.primary,
+        accentColor: ExpenseTrackerColors.violet,
         brightness: Brightness.dark,
       ),
-      appBarTheme: _appBarTheme,
+      // appBarTheme: _appBarTheme,
       elevatedButtonTheme: _elevatedButtonTheme,
       outlinedButtonTheme: _outlinedButtonTheme,
-      textTheme: _textTheme,
-      dialogBackgroundColor: ExpenseTrackerColors.darkBackground,
-      dialogTheme: _dialogTheme,
-      tooltipTheme: _tooltipTheme,
-      bottomSheetTheme: _bottomSheetTheme,
-      tabBarTheme: _tabBarTheme,
-      dividerTheme: _dividerTheme,
+      // textTheme: _textTheme,
+      // // dialogBackgroundColor: ExpenseTrackerColors.darkBackground,
+      // dialogTheme: _dialogTheme,
+      // tooltipTheme: _tooltipTheme,
+      // bottomSheetTheme: _bottomSheetTheme,
+      // tabBarTheme: _tabBarTheme,
+      // dividerTheme: _dividerTheme,
     );
   }
 
-  /// `ThemeData` for ExpenseTracker UI for small screens.
-  static ThemeData get small {
-    return standard.copyWith(textTheme: _smallTextTheme);
-  }
+  // /// `ThemeData` for ExpenseTracker UI for small screens.
+  // static ThemeData get small {
+  //   return standard.copyWith(textTheme: _smallTextTheme);
+  // }
 
-  /// `ThemeData` for ExpenseTracker UI for medium screens.
-  static ThemeData get medium {
-    return standard.copyWith(textTheme: _smallTextTheme);
-  }
+  // /// `ThemeData` for ExpenseTracker UI for medium screens.
+  // static ThemeData get medium {
+  //   return standard.copyWith(textTheme: _smallTextTheme);
+  // }
 
-  /// `ThemeData` for ExpenseTracker UI for large screens.
-  static ThemeData get large {
-    return standard.copyWith(textTheme: _largeTextTheme);
-  }
+  // /// `ThemeData` for ExpenseTracker UI for large screens.
+  // static ThemeData get large {
+  //   return standard.copyWith(textTheme: _largeTextTheme);
+  // }
 
-  static TextTheme get _textTheme {
-    return TextTheme(
-      displayLarge: ExpenseTrackerTextStyle.headline1,
-      displayMedium: ExpenseTrackerTextStyle.headline2,
-      displaySmall: ExpenseTrackerTextStyle.headline3,
-      headlineMedium: ExpenseTrackerTextStyle.headline4,
-      headlineSmall: ExpenseTrackerTextStyle.headline5,
-      titleLarge: ExpenseTrackerTextStyle.headline6,
-      titleMedium: ExpenseTrackerTextStyle.subtitle1,
-      titleSmall: ExpenseTrackerTextStyle.subtitle2,
-      bodyLarge: ExpenseTrackerTextStyle.bodyText1,
-      bodyMedium: ExpenseTrackerTextStyle.bodyText2,
-      bodySmall: ExpenseTrackerTextStyle.caption,
-      labelSmall: ExpenseTrackerTextStyle.overline,
-      labelLarge: ExpenseTrackerTextStyle.button,
-    );
-  }
+  // static TextTheme get _textTheme {
+  //   return TextTheme(
+  //     displayLarge: ExpenseTrackerTextStyle.headline1,
+  //     displayMedium: ExpenseTrackerTextStyle.headline2,
+  //     displaySmall: ExpenseTrackerTextStyle.headline3,
+  //     headlineMedium: ExpenseTrackerTextStyle.headline4,
+  //     headlineSmall: ExpenseTrackerTextStyle.headline5,
+  //     titleLarge: ExpenseTrackerTextStyle.headline6,
+  //     titleMedium: ExpenseTrackerTextStyle.subtitle1,
+  //     titleSmall: ExpenseTrackerTextStyle.subtitle2,
+  //     bodyLarge: ExpenseTrackerTextStyle.bodyText1,
+  //     bodyMedium: ExpenseTrackerTextStyle.bodyText2,
+  //     bodySmall: ExpenseTrackerTextStyle.caption,
+  //     labelSmall: ExpenseTrackerTextStyle.overline,
+  //     labelLarge: ExpenseTrackerTextStyle.button,
+  //   );
+  // }
 
-  static TextTheme get _smallTextTheme {
-    return TextTheme(
-      displayLarge: ExpenseTrackerTextStyle.headline1.copyWith(
-        fontSize: _textTheme.displayLarge!.fontSize! * _smallTextScaleFactor,
-      ),
-      displayMedium: ExpenseTrackerTextStyle.headline2.copyWith(
-        fontSize: _textTheme.displayMedium!.fontSize! * _smallTextScaleFactor,
-      ),
-      displaySmall: ExpenseTrackerTextStyle.headline3.copyWith(
-        fontSize: _textTheme.displaySmall!.fontSize! * _smallTextScaleFactor,
-      ),
-      headlineMedium: ExpenseTrackerTextStyle.headline4.copyWith(
-        fontSize: _textTheme.headlineMedium!.fontSize! * _smallTextScaleFactor,
-      ),
-      headlineSmall: ExpenseTrackerTextStyle.headline5.copyWith(
-        fontSize: _textTheme.headlineSmall!.fontSize! * _smallTextScaleFactor,
-      ),
-      titleLarge: ExpenseTrackerTextStyle.headline6.copyWith(
-        fontSize: _textTheme.titleLarge!.fontSize! * _smallTextScaleFactor,
-      ),
-      titleMedium: ExpenseTrackerTextStyle.subtitle1.copyWith(
-        fontSize: _textTheme.titleMedium!.fontSize! * _smallTextScaleFactor,
-      ),
-      titleSmall: ExpenseTrackerTextStyle.subtitle2.copyWith(
-        fontSize: _textTheme.titleSmall!.fontSize! * _smallTextScaleFactor,
-      ),
-      bodyLarge: ExpenseTrackerTextStyle.bodyText1.copyWith(
-        fontSize: _textTheme.bodyLarge!.fontSize! * _smallTextScaleFactor,
-      ),
-      bodyMedium: ExpenseTrackerTextStyle.bodyText2.copyWith(
-        fontSize: _textTheme.bodyMedium!.fontSize! * _smallTextScaleFactor,
-      ),
-      bodySmall: ExpenseTrackerTextStyle.caption.copyWith(
-        fontSize: _textTheme.bodySmall!.fontSize! * _smallTextScaleFactor,
-      ),
-      labelSmall: ExpenseTrackerTextStyle.overline.copyWith(
-        fontSize: _textTheme.labelSmall!.fontSize! * _smallTextScaleFactor,
-      ),
-      labelLarge: ExpenseTrackerTextStyle.button.copyWith(
-        fontSize: _textTheme.labelLarge!.fontSize! * _smallTextScaleFactor,
-      ),
-    );
-  }
+  // static TextTheme get _smallTextTheme {
+  //   return TextTheme(
+  //     displayLarge: ExpenseTrackerTextStyle.headline1.copyWith(
+  //       fontSize: _textTheme.displayLarge!.fontSize! * _smallTextScaleFactor,
+  //     ),
+  //     displayMedium: ExpenseTrackerTextStyle.headline2.copyWith(
+  //       fontSize: _textTheme.displayMedium!.fontSize! * _smallTextScaleFactor,
+  //     ),
+  //     displaySmall: ExpenseTrackerTextStyle.headline3.copyWith(
+  //       fontSize: _textTheme.displaySmall!.fontSize! * _smallTextScaleFactor,
+  //     ),
+  //     headlineMedium: ExpenseTrackerTextStyle.headline4.copyWith(
+  //       fontSize: _textTheme.headlineMedium!.fontSize! * _smallTextScaleFactor,
+  //     ),
+  //     headlineSmall: ExpenseTrackerTextStyle.headline5.copyWith(
+  //       fontSize: _textTheme.headlineSmall!.fontSize! * _smallTextScaleFactor,
+  //     ),
+  //     titleLarge: ExpenseTrackerTextStyle.headline6.copyWith(
+  //       fontSize: _textTheme.titleLarge!.fontSize! * _smallTextScaleFactor,
+  //     ),
+  //     titleMedium: ExpenseTrackerTextStyle.subtitle1.copyWith(
+  //       fontSize: _textTheme.titleMedium!.fontSize! * _smallTextScaleFactor,
+  //     ),
+  //     titleSmall: ExpenseTrackerTextStyle.subtitle2.copyWith(
+  //       fontSize: _textTheme.titleSmall!.fontSize! * _smallTextScaleFactor,
+  //     ),
+  //     bodyLarge: ExpenseTrackerTextStyle.bodyText1.copyWith(
+  //       fontSize: _textTheme.bodyLarge!.fontSize! * _smallTextScaleFactor,
+  //     ),
+  //     bodyMedium: ExpenseTrackerTextStyle.bodyText2.copyWith(
+  //       fontSize: _textTheme.bodyMedium!.fontSize! * _smallTextScaleFactor,
+  //     ),
+  //     bodySmall: ExpenseTrackerTextStyle.caption.copyWith(
+  //       fontSize: _textTheme.bodySmall!.fontSize! * _smallTextScaleFactor,
+  //     ),
+  //     labelSmall: ExpenseTrackerTextStyle.overline.copyWith(
+  //       fontSize: _textTheme.labelSmall!.fontSize! * _smallTextScaleFactor,
+  //     ),
+  //     labelLarge: ExpenseTrackerTextStyle.button.copyWith(
+  //       fontSize: _textTheme.labelLarge!.fontSize! * _smallTextScaleFactor,
+  //     ),
+  //   );
+  // }
 
-  static TextTheme get _largeTextTheme {
-    return TextTheme(
-      displayLarge: ExpenseTrackerTextStyle.headline1.copyWith(
-        fontSize: _textTheme.displayLarge!.fontSize! * _largeTextScaleFactor,
-      ),
-      displayMedium: ExpenseTrackerTextStyle.headline2.copyWith(
-        fontSize: _textTheme.displayMedium!.fontSize! * _largeTextScaleFactor,
-      ),
-      displaySmall: ExpenseTrackerTextStyle.headline3.copyWith(
-        fontSize: _textTheme.displaySmall!.fontSize! * _largeTextScaleFactor,
-      ),
-      headlineMedium: ExpenseTrackerTextStyle.headline4.copyWith(
-        fontSize: _textTheme.headlineMedium!.fontSize! * _largeTextScaleFactor,
-      ),
-      headlineSmall: ExpenseTrackerTextStyle.headline5.copyWith(
-        fontSize: _textTheme.headlineSmall!.fontSize! * _largeTextScaleFactor,
-      ),
-      titleLarge: ExpenseTrackerTextStyle.headline6.copyWith(
-        fontSize: _textTheme.titleLarge!.fontSize! * _largeTextScaleFactor,
-      ),
-      titleMedium: ExpenseTrackerTextStyle.subtitle1.copyWith(
-        fontSize: _textTheme.titleMedium!.fontSize! * _largeTextScaleFactor,
-      ),
-      titleSmall: ExpenseTrackerTextStyle.subtitle2.copyWith(
-        fontSize: _textTheme.titleSmall!.fontSize! * _largeTextScaleFactor,
-      ),
-      bodyLarge: ExpenseTrackerTextStyle.bodyText1.copyWith(
-        fontSize: _textTheme.bodyLarge!.fontSize! * _largeTextScaleFactor,
-      ),
-      bodyMedium: ExpenseTrackerTextStyle.bodyText2.copyWith(
-        fontSize: _textTheme.bodyMedium!.fontSize! * _largeTextScaleFactor,
-      ),
-      bodySmall: ExpenseTrackerTextStyle.caption.copyWith(
-        fontSize: _textTheme.bodySmall!.fontSize! * _largeTextScaleFactor,
-      ),
-      labelSmall: ExpenseTrackerTextStyle.overline.copyWith(
-        fontSize: _textTheme.labelSmall!.fontSize! * _largeTextScaleFactor,
-      ),
-      labelLarge: ExpenseTrackerTextStyle.button.copyWith(
-        fontSize: _textTheme.labelLarge!.fontSize! * _largeTextScaleFactor,
-      ),
-    );
-  }
+  // static TextTheme get _largeTextTheme {
+  //   return TextTheme(
+  //     displayLarge: ExpenseTrackerTextStyle.headline1.copyWith(
+  //       fontSize: _textTheme.displayLarge!.fontSize! * _largeTextScaleFactor,
+  //     ),
+  //     displayMedium: ExpenseTrackerTextStyle.headline2.copyWith(
+  //       fontSize: _textTheme.displayMedium!.fontSize! * _largeTextScaleFactor,
+  //     ),
+  //     displaySmall: ExpenseTrackerTextStyle.headline3.copyWith(
+  //       fontSize: _textTheme.displaySmall!.fontSize! * _largeTextScaleFactor,
+  //     ),
+  //     headlineMedium: ExpenseTrackerTextStyle.headline4.copyWith(
+  //       fontSize: _textTheme.headlineMedium!.fontSize! * _largeTextScaleFactor,
+  //     ),
+  //     headlineSmall: ExpenseTrackerTextStyle.headline5.copyWith(
+  //       fontSize: _textTheme.headlineSmall!.fontSize! * _largeTextScaleFactor,
+  //     ),
+  //     titleLarge: ExpenseTrackerTextStyle.headline6.copyWith(
+  //       fontSize: _textTheme.titleLarge!.fontSize! * _largeTextScaleFactor,
+  //     ),
+  //     titleMedium: ExpenseTrackerTextStyle.subtitle1.copyWith(
+  //       fontSize: _textTheme.titleMedium!.fontSize! * _largeTextScaleFactor,
+  //     ),
+  //     titleSmall: ExpenseTrackerTextStyle.subtitle2.copyWith(
+  //       fontSize: _textTheme.titleSmall!.fontSize! * _largeTextScaleFactor,
+  //     ),
+  //     bodyLarge: ExpenseTrackerTextStyle.bodyText1.copyWith(
+  //       fontSize: _textTheme.bodyLarge!.fontSize! * _largeTextScaleFactor,
+  //     ),
+  //     bodyMedium: ExpenseTrackerTextStyle.bodyText2.copyWith(
+  //       fontSize: _textTheme.bodyMedium!.fontSize! * _largeTextScaleFactor,
+  //     ),
+  //     bodySmall: ExpenseTrackerTextStyle.caption.copyWith(
+  //       fontSize: _textTheme.bodySmall!.fontSize! * _largeTextScaleFactor,
+  //     ),
+  //     labelSmall: ExpenseTrackerTextStyle.overline.copyWith(
+  //       fontSize: _textTheme.labelSmall!.fontSize! * _largeTextScaleFactor,
+  //     ),
+  //     labelLarge: ExpenseTrackerTextStyle.button.copyWith(
+  //       fontSize: _textTheme.labelLarge!.fontSize! * _largeTextScaleFactor,
+  //     ),
+  //   );
+  // }
 
-  static AppBarTheme get _appBarTheme {
-    return const AppBarTheme(
-      color: ExpenseTrackerColors.primary,
-      centerTitle: true,
-    );
-  }
+  // static AppBarTheme get _appBarTheme {
+  //   return const AppBarTheme(
+  //     color: ExpenseTrackerColors.primary,
+  //     centerTitle: true,
+  //   );
+  // }
 
   static ElevatedButtonThemeData get _elevatedButtonTheme {
     return ElevatedButtonThemeData(
       style: ElevatedButton.styleFrom(
         elevation: 0,
-        backgroundColor: ExpenseTrackerColors.primary,
+        foregroundColor: Colors.white,
+        backgroundColor: ExpenseTrackerColors.violet,
         shape: const RoundedRectangleBorder(
-          borderRadius: BorderRadius.all(Radius.circular(30)),
+          borderRadius: ExpenseTrackerTheme.borderRadiusSmall,
         ),
-        fixedSize: const Size(208, 54),
+        minimumSize: const Size(double.infinity, 48),
       ),
     );
   }
@@ -192,71 +191,72 @@ class ExpenseTrackerTheme {
   static OutlinedButtonThemeData get _outlinedButtonTheme {
     return OutlinedButtonThemeData(
       style: OutlinedButton.styleFrom(
-        foregroundColor: ExpenseTrackerColors.white,
+        foregroundColor: ExpenseTrackerColors.violet,
+        backgroundColor: ExpenseTrackerColors.violet20,
         shape: const RoundedRectangleBorder(
-          borderRadius: BorderRadius.all(Radius.circular(30)),
+          borderRadius: ExpenseTrackerTheme.borderRadiusSmall,
         ),
-        side: const BorderSide(color: ExpenseTrackerColors.white, width: 2),
-        fixedSize: const Size(208, 54),
+        // side: const BorderSide(color: ExpenseTrackerColors.white, width: 2),
+        minimumSize: const Size(double.infinity, 48),
       ),
     );
   }
 
-  static TooltipThemeData get _tooltipTheme {
-    return const TooltipThemeData(
-      decoration: BoxDecoration(
-        color: ExpenseTrackerColors.charcoal,
-        borderRadius: BorderRadius.all(Radius.circular(5)),
-      ),
-      padding: EdgeInsets.all(10),
-      textStyle: TextStyle(color: ExpenseTrackerColors.white),
-    );
-  }
+  // static TooltipThemeData get _tooltipTheme {
+  //   return const TooltipThemeData(
+  //     decoration: BoxDecoration(
+  //       color: ExpenseTrackerColors.charcoal,
+  //       borderRadius: BorderRadius.all(Radius.circular(5)),
+  //     ),
+  //     padding: EdgeInsets.all(10),
+  //     textStyle: TextStyle(color: ExpenseTrackerColors.white),
+  //   );
+  // }
 
-  static DialogTheme get _dialogTheme {
-    return DialogTheme(
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(12),
-      ),
-    );
-  }
+  // static DialogTheme get _dialogTheme {
+  //   return DialogTheme(
+  //     shape: RoundedRectangleBorder(
+  //       borderRadius: BorderRadius.circular(12),
+  //     ),
+  //   );
+  // }
 
-  static BottomSheetThemeData get _bottomSheetTheme {
-    return const BottomSheetThemeData(
-      backgroundColor: ExpenseTrackerColors.whiteBackground,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.vertical(top: Radius.circular(12)),
-      ),
-    );
-  }
+  // static BottomSheetThemeData get _bottomSheetTheme {
+  //   return const BottomSheetThemeData(
+  //     backgroundColor: ExpenseTrackerColors.whiteBackground,
+  //     shape: RoundedRectangleBorder(
+  //       borderRadius: BorderRadius.vertical(top: Radius.circular(12)),
+  //     ),
+  //   );
+  // }
 
-  static TabBarTheme get _tabBarTheme {
-    return const TabBarTheme(
-      indicator: UnderlineTabIndicator(
-        borderSide: BorderSide(
-          width: 2,
-          color: ExpenseTrackerColors.primary,
-        ),
-      ),
-      labelColor: ExpenseTrackerColors.primary,
-      unselectedLabelColor: ExpenseTrackerColors.black25,
-      indicatorSize: TabBarIndicatorSize.tab,
-    );
-  }
+  // static TabBarTheme get _tabBarTheme {
+  //   return const TabBarTheme(
+  //     indicator: UnderlineTabIndicator(
+  //       borderSide: BorderSide(
+  //         width: 2,
+  //         color: ExpenseTrackerColors.primary,
+  //       ),
+  //     ),
+  //     labelColor: ExpenseTrackerColors.primary,
+  //     unselectedLabelColor: ExpenseTrackerColors.black25,
+  //     indicatorSize: TabBarIndicatorSize.tab,
+  //   );
+  // }
 
-  static DividerThemeData get _dividerTheme {
-    return const DividerThemeData(
-      space: 0,
-      thickness: 1,
-      color: ExpenseTrackerColors.black25,
-    );
-  }
+  // static DividerThemeData get _dividerTheme {
+  //   return const DividerThemeData(
+  //     space: 0,
+  //     thickness: 1,
+  //     color: ExpenseTrackerColors.black25,
+  //   );
+  // }
 
   /// border radius [small]
   static const borderRadiusSmall = BorderRadius.all(Radius.circular(10));
 
   /// border radius [medium]
-  static const borderRadiusMedium = BorderRadius.all(Radius.circular(15));
+  static const borderRadiusMedium = BorderRadius.all(Radius.circular(16));
 
   /// border radius [large]
   static const borderRadiusLarge = BorderRadius.all(Radius.circular(20));
