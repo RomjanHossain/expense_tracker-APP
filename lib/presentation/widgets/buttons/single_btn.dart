7c11350421eef63d8c9f3760cdff7f24330efb97
RomjanHossain <romjan@skiff.com>
2023-12-24 06:55:26 +0600

    Update button color and text style

--- lib/presentation/widgets/buttons/single_btn.dart
+++ lib/presentation/widgets/buttons/single_btn.dart
@@ -7,7 +7,6 @@ class Button extends StatelessWidget {
     required this.cb,
     super.key,
     this.big = false,
-    this.color = Default,
   });
 
   const Button.big({
@@ -15,7 +14,6 @@ class Button extends StatelessWidget {
     required this.cb,
     super.key,
     this.big = true,
-    this.color = Default,
   });
 
   const Button.operation({
@@ -23,15 +21,10 @@ class Button extends StatelessWidget {
     required this.cb,
     super.key,
     this.big = false,
-    this.color = Operation,
   });
-  static const Dark = Color.fromARGB(255, 145, 145, 145);
-  static const Default = Color.fromARGB(255, 40, 40, 40);
-  static const Operation = Color.fromARGB(255, 13, 203, 250);
 
   final String text;
   final bool big;
-  final Color color;
   final void Function(String) cb;
 
   @override
@@ -41,15 +34,15 @@ class Button extends StatelessWidget {
       child: Container(
         margin: const EdgeInsets.all(5),
         decoration: BoxDecoration(
-          color: ExpenseTrackerColors.charcoal,
+          color: ExpenseTrackerColors.violet,
           borderRadius: BorderRadius.circular(16),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.white.withOpacity(0.24),
-              blurRadius: 1,
-              spreadRadius: 1,
-            ),
-          ],
+          // boxShadow: [
+          //   BoxShadow(
+          //     color: Colors.white.withOpacity(0.24),
+          //     blurRadius: 1,
+          //     spreadRadius: 1,
+          //   ),
+          // ],
         ),
         child: ElevatedButton(
           style: ElevatedButton.styleFrom(
@@ -58,6 +51,11 @@ class Button extends StatelessWidget {
           ),
           child: Text(
             text,
+            style: ExpenseTrackerTextStyle.title1.copyWith(
+              color: ExpenseTrackerColors.light80,
+              // fontWeight: FontWeight.w600,
+              fontSize: 48,
+            ),
             // style: ExpenseTrackerTextStyle.headline2.copyWith(
             //   color: color,
             //   fontWeight: FontWeight.w600,
