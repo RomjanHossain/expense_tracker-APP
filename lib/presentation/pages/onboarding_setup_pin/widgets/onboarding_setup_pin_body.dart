15e921b2854163c7da8e64bb61fad430129547c7
RomjanHossain <romjan@skiff.com>
2023-12-24 06:55:12 +0600

    Refactor PIN setup UI

--- lib/presentation/pages/onboarding_setup_pin/widgets/onboarding_setup_pin_body.dart
+++ lib/presentation/pages/onboarding_setup_pin/widgets/onboarding_setup_pin_body.dart
@@ -1,3 +1,4 @@
+import 'package:expense_tracker/app/ui/app_ui.dart';
 import 'package:expense_tracker/presentation/cubit/expense_text_controller_cubit.dart';
 import 'package:expense_tracker/presentation/pages/onboarding_setup_pin/bloc/bloc.dart';
 import 'package:expense_tracker/presentation/widgets/buttons/input_btn.dart';
@@ -15,115 +16,128 @@ class OnboardingSetupPinBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return BlocBuilder<OnboardingSetupPinBloc, OnboardingSetupPinState>(
       builder: (context, state) {
         return Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text('Let\'s setup your PIN'),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 20),
+              child: Text(
+                "Let's setup your PIN",
+                style: ExpenseTrackerTextStyle.title3.copyWith(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
             Row(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 for (int i = 0; i < 4; i++)
-                  Container(
-                    width: 50,
-                    height: 50,
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    child: const Center(
-                      child: Text(
-                        '*',
-                        style: TextStyle(
-                          fontSize: 30,
-                          fontWeight: FontWeight.bold,
-                        ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: CircleAvatar(
+                      backgroundColor: Color(0xffeee5ff),
+                      child: CircleAvatar(
+                        radius: 12,
+                        backgroundColor: ExpenseTrackerColors.violet,
                       ),
+                      radius: 15,
                     ),
                   ),
               ],
             ),
-            const SizedBox(height: 20),
+            const SizedBox(height: 50),
             // numbers
-            Expanded(
+            SizedBox(
+                height: size.height * 0.4,
                 child: Row(
-              children: [
-                // 1, 4, 7 and $
-                ButtonRow([
-                  Button(
-                    text: '1',
-                    cb: (s) {
-                      context.read<ExpenseTextControllerCubit>().addText(s);
-                    },
-                  ),
-                  Button(
-                    text: '4',
-                    cb: (s) {
-                      context.read<ExpenseTextControllerCubit>().addText(s);
-                    },
-                  ),
-                  Button(
-                    text: '7',
-                    cb: (s) {
-                      context.read<ExpenseTextControllerCubit>().addText(s);
-                    },
-                  ),
-                  Button(
-                    text: r'$',
-                    cb: (s) {
-                      context.read<ExpenseTextControllerCubit>().addText(s);
-                    },
-                  ),
-                ]),
-                // 2, 5, 8 and 0
-                ButtonRow([
-                  Button(
-                    text: '2',
-                    cb: (s) {
-                      context.read<ExpenseTextControllerCubit>().addText(s);
-                    },
-                  ),
-                  Button(
-                    text: '5',
-                    cb: (s) {
-                      context.read<ExpenseTextControllerCubit>().addText(s);
-                    },
-                  ),
-                  Button(
-                    text: '8',
-                    cb: (s) {
-                      context.read<ExpenseTextControllerCubit>().addText(s);
-                    },
-                  ),
-                  Button(
-                    text: '0',
-                    cb: (s) {
-                      context.read<ExpenseTextControllerCubit>().addText(s);
-                    },
-                  ),
-                ]),
-                // 3, 6, 9 and delete
-                ButtonRow([
-                  Button(
-                    text: '3',
-                    cb: (s) {
-                      context.read<ExpenseTextControllerCubit>().addText(s);
-                    },
-                  ),
-                  Button(
-                    text: '6',
-                    cb: (s) {
-                      context.read<ExpenseTextControllerCubit>().addText(s);
-                    },
-                  ),
-                  Button(
-                    text: '9',
-                    cb: (s) {
-                      context.read<ExpenseTextControllerCubit>().addText(s);
-                    },
-                  ),
-                ]),
-              ],
-            )),
+                  children: [
+                    // 1, 4, 7 and $
+                    ButtonRow([
+                      Button(
+                        text: '1',
+                        cb: (s) {
+                          context.read<ExpenseTextControllerCubit>().addText(s);
+                        },
+                      ),
+                      Button(
+                        text: '4',
+                        cb: (s) {
+                          context.read<ExpenseTextControllerCubit>().addText(s);
+                        },
+                      ),
+                      Button(
+                        text: '7',
+                        cb: (s) {
+                          context.read<ExpenseTextControllerCubit>().addText(s);
+                        },
+                      ),
+                      Button(
+                        text: '',
+                        cb: (s) {
+                          // context.read<ExpenseTextControllerCubit>().addText(s);
+                        },
+                      ),
+                    ]),
+                    // 2, 5, 8 and 0
+                    ButtonRow([
+                      Button(
+                        text: '2',
+                        cb: (s) {
+                          context.read<ExpenseTextControllerCubit>().addText(s);
+                        },
+                      ),
+                      Button(
+                        text: '5',
+                        cb: (s) {
+                          context.read<ExpenseTextControllerCubit>().addText(s);
+                        },
+                      ),
+                      Button(
+                        text: '8',
+                        cb: (s) {
+                          context.read<ExpenseTextControllerCubit>().addText(s);
+                        },
+                      ),
+                      Button(
+                        text: '0',
+                        cb: (s) {
+                          context.read<ExpenseTextControllerCubit>().addText(s);
+                        },
+                      ),
+                    ]),
+                    // 3, 6, 9 and delete
+                    ButtonRow([
+                      Button(
+                        text: '3',
+                        cb: (s) {
+                          context.read<ExpenseTextControllerCubit>().addText(s);
+                        },
+                      ),
+                      Button(
+                        text: '6',
+                        cb: (s) {
+                          context.read<ExpenseTextControllerCubit>().addText(s);
+                        },
+                      ),
+                      Button(
+                        text: '9',
+                        cb: (s) {
+                          context.read<ExpenseTextControllerCubit>().addText(s);
+                        },
+                      ),
+                      Button(
+                        text: '->',
+                        cb: (s) {
+                          context.read<ExpenseTextControllerCubit>().addText(s);
+                        },
+                      ),
+                    ]),
+                  ],
+                )),
           ],
         );
       },
