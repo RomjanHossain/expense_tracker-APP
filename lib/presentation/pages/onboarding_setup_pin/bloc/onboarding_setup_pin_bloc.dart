ebd0b51e643762e28184a54a3de1aa98ff0b2df8
RomjanHossain <romjan@skiff.com>
2023-12-25 08:25:16 +0600

    Add text, remove text, and clear text functionality in OnboardingSetupPinBloc

--- lib/presentation/pages/onboarding_setup_pin/bloc/onboarding_setup_pin_bloc.dart
+++ lib/presentation/pages/onboarding_setup_pin/bloc/onboarding_setup_pin_bloc.dart
@@ -2,18 +2,47 @@ import 'dart:async';
 
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
 part 'onboarding_setup_pin_event.dart';
 part 'onboarding_setup_pin_state.dart';
 
-class OnboardingSetupPinBloc extends Bloc<OnboardingSetupPinEvent, OnboardingSetupPinState> {
+class OnboardingSetupPinBloc
+    extends Bloc<OnboardingSetupPinEvent, OnboardingSetupPinState> {
   OnboardingSetupPinBloc() : super(const OnboardingSetupPinInitial()) {
-    on<CustomOnboardingSetupPinEvent>(_onCustomOnboardingSetupPinEvent);
+    on<AddTextOnboardingSetupPinEvent>(_onAddTextOnboardingSetupPinEvent);
+    on<RemoveTextOnboardingSetupPinEvent>(_onRemoveTextOnboardingSetupPinEvent);
+    on<ClearTextOnboardingSetupPinEvent>(_onClearTextOnboardingSetupPinEvent);
   }
 
-  FutureOr<void> _onCustomOnboardingSetupPinEvent(
-    CustomOnboardingSetupPinEvent event,
+  // add text
+  FutureOr<void> _onAddTextOnboardingSetupPinEvent(
+    AddTextOnboardingSetupPinEvent event,
     Emitter<OnboardingSetupPinState> emit,
   ) {
-    // TODO: Add Logic
+    debugPrint('pin: ${state.pin}');
+    if (state.pin.length > 3) {
+      debugPrint('pin is more than 4');
+      emit(const OnboardingSetupPinError());
+      emit(const OnboardingSetupPinInitial());
+    } else {
+      debugPrint('pin is less than 4');
+      emit(state.copyWith(pin: state.pin + event.pin));
+    }
+  }
+
+  // remove text
+  FutureOr<void> _onRemoveTextOnboardingSetupPinEvent(
+    RemoveTextOnboardingSetupPinEvent event,
+    Emitter<OnboardingSetupPinState> emit,
+  ) {
+    emit(state.copyWith(pin: state.pin.substring(0, state.pin.length - 1)));
+  }
+
+  // clear text
+  FutureOr<void> _onClearTextOnboardingSetupPinEvent(
+    ClearTextOnboardingSetupPinEvent event,
+    Emitter<OnboardingSetupPinState> emit,
+  ) {
+    emit(state.copyWith(pin: ''));
   }
 }
