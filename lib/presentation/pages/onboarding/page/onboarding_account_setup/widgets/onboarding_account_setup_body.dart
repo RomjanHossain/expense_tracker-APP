0ea2ad37ad8ccd9e4e8a1bf3820cda9661b5c369
RomjanHossain <romjan@skiff.com>
2023-12-25 23:40:43 +0600

    Update OnboardingAccountSetupBody widget

--- lib/presentation/pages/onboarding/page/onboarding_account_setup/widgets/onboarding_account_setup_body.dart
+++ lib/presentation/pages/onboarding/page/onboarding_account_setup/widgets/onboarding_account_setup_body.dart
@@ -1,5 +1,9 @@
-import 'package:flutter/material.dart';
+import 'package:expense_tracker/app/ui/app_ui.dart';
+import 'package:expense_tracker/app/ui/src/typography/text_styles.dart';
 import 'package:expense_tracker/presentation/pages/onboarding/page/onboarding_account_setup/bloc/bloc.dart';
+import 'package:expense_tracker/presentation/pages/onboarding/page/onboarding_account_setup/components/add_account_bottom_container.dart';
+import 'package:expense_tracker/presentation/widgets/buttons/buttons.dart';
+import 'package:flutter/material.dart';
 
 /// {@template onboarding_account_setup_body}
 /// Body of the OnboardingAccountSetupPage.
@@ -14,7 +18,24 @@ class OnboardingAccountSetupBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocBuilder<OnboardingAccountSetupBloc, OnboardingAccountSetupState>(
       builder: (context, state) {
-        return Center(child: Text(state.customProperty));
+        return Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Text(
+              'Balance',
+              style: ExpenseTrackerTextStyle.title3.copyWith(
+                color: ExpenseTrackerColors.light80.withOpacity(0.64),
+              ),
+            ),
+            Text(
+              r'$0.00',
+              style:
+                  ExpenseTrackerTextStyle.titleX.copyWith(color: Colors.white),
+            ),
+            const AddAccountBottomContainer(),
+          ],
+        );
       },
     );
   }
