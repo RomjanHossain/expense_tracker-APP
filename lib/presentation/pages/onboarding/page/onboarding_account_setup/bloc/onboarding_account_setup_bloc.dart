71a2b24974472ba7f62559ea77115103adfaca75
RomjanHossain <romjan@skiff.com>
2023-12-27 00:02:32 +0600

    Add account setup events and handlers

--- lib/presentation/pages/onboarding/page/onboarding_account_setup/bloc/onboarding_account_setup_bloc.dart
+++ lib/presentation/pages/onboarding/page/onboarding_account_setup/bloc/onboarding_account_setup_bloc.dart
@@ -1,13 +1,55 @@
 import 'dart:async';
-
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:expense_tracker/data/datasources/local/utils_data/local_ac_type.dart';
+import 'package:expense_tracker/domain/entities/create_account_entity/create_account_entity.dart';
 part 'onboarding_account_setup_event.dart';
 part 'onboarding_account_setup_state.dart';
 
-class OnboardingAccountSetupBloc extends Bloc<OnboardingAccountSetupEvent, OnboardingAccountSetupState> {
+class OnboardingAccountSetupBloc
+    extends Bloc<OnboardingAccountSetupEvent, OnboardingAccountSetupState> {
   OnboardingAccountSetupBloc() : super(const OnboardingAccountSetupInitial()) {
     on<CustomOnboardingAccountSetupEvent>(_onCustomOnboardingAccountSetupEvent);
+    on<AddAccountLogoEvent>(_onAddAccountLogoEvent);
+    on<AddAccountNameEvent>(_onAddAccountNameEvent);
+    on<AddAccountTypeEvent>(_onAddAccountTypeEvent);
+    on<AddBalanceEvent>(_onAddBalanceEvent);
+  }
+
+  FutureOr<void> _onAddBalanceEvent(
+    AddBalanceEvent event,
+    Emitter<OnboardingAccountSetupState> emit,
+  ) {
+    emit(state . copyWith(
+      balance: event.balance,
+    ));
+  }
+
+  FutureOr<void> _onAddAccountTypeEvent(
+    AddAccountTypeEvent event,
+    Emitter<OnboardingAccountSetupState> emit,
+  ) {
+    emit(state . copyWith(
+      accountType: event.accountType,
+    ));
+  }
+
+  FutureOr<void> _onAddAccountNameEvent(
+    AddAccountNameEvent event,
+    Emitter<OnboardingAccountSetupState> emit,
+  ) {
+    emit(state . copyWith(
+      accountName: event.accountName,
+    ));
+  }
+
+  FutureOr<void> _onAddAccountLogoEvent(
+    AddAccountLogoEvent event,
+    Emitter<OnboardingAccountSetupState> emit,
+  ) {
+    emit(state . copyWith(
+      accountLogo: event.accountLogo,
+    ));
   }
 
   FutureOr<void> _onCustomOnboardingAccountSetupEvent(
