2fa21d505c970cb0d27b3c76e7fe3a3ffbf3eac7
RomjanHossain <romjan@skiff.com>
2023-12-26 08:14:59 +0600

    Add mobile banking icons

---
+++ lib/data/datasources/local/utils_data/mobile_banking_db.dart
@@ -0,0 +1,13 @@
+import 'package:expense_tracker/app/ui/src/assets/assets_icons_n_illustration.dart';
+
+Map<String, String> get mobileBanking => {
+      'Bkash': ExpenseAssets.bkashIcon,
+      'Nagad': ExpenseAssets.nagadIcon,
+      'Rocket': ExpenseAssets.rocketIcon,
+      'Mkash': ExpenseAssets.mCashIcon,
+      'SureCash': ExpenseAssets.sureCashIcon,
+      'Ucash': ExpenseAssets.ucashIcon,
+      'Upay': ExpenseAssets.upayIcon,
+      'Ok': ExpenseAssets.okIcon,
+      'Tcash': ExpenseAssets.tCashIcon,
+    };
