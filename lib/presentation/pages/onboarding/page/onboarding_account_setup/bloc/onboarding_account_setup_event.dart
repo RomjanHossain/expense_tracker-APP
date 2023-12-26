ca65ae4731d21ed32d52c0161e5417c60d0802fb
RomjanHossain <romjan@skiff.com>
2023-12-27 00:02:38 +0600

    Add new events for onboarding account setup

--- lib/presentation/pages/onboarding/page/onboarding_account_setup/bloc/onboarding_account_setup_event.dart
+++ lib/presentation/pages/onboarding/page/onboarding_account_setup/bloc/onboarding_account_setup_event.dart
@@ -15,3 +15,26 @@ class CustomOnboardingAccountSetupEvent extends OnboardingAccountSetupEvent {
   /// {@macro custom_onboarding_account_setup_event}
   const CustomOnboardingAccountSetupEvent();
 }
+
+/// add balance event
+class AddBalanceEvent extends OnboardingAccountSetupEvent {
+  const AddBalanceEvent(this.balance);
+  final double balance;
+}
+
+/// add account name event
+class AddAccountNameEvent extends OnboardingAccountSetupEvent {
+  const AddAccountNameEvent(this.accountName);
+  final String accountName;
+}
+/// add account type event
+class AddAccountTypeEvent extends OnboardingAccountSetupEvent {
+  const AddAccountTypeEvent(this.accountType);
+  final AccountType accountType;
+}
+
+/// add account logo
+class AddAccountLogoEvent extends OnboardingAccountSetupEvent {
+  const AddAccountLogoEvent(this.accountLogo);
+  final String accountLogo;
+}
