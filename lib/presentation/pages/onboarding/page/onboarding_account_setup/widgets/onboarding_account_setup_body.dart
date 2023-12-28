b503a5dc28da045f7a8053d75d94612a8b41ee02
RomjanHossain <romjan@skiff.com>
2023-12-28 08:05:08 +0600

    Refactor onboarding account setup body widget

--- lib/presentation/pages/onboarding/page/onboarding_account_setup/widgets/onboarding_account_setup_body.dart
+++ lib/presentation/pages/onboarding/page/onboarding_account_setup/widgets/onboarding_account_setup_body.dart
@@ -1,8 +1,6 @@
 import 'package:expense_tracker/app/ui/app_ui.dart';
-import 'package:expense_tracker/app/ui/src/typography/text_styles.dart';
 import 'package:expense_tracker/presentation/pages/onboarding/page/onboarding_account_setup/bloc/bloc.dart';
 import 'package:expense_tracker/presentation/pages/onboarding/page/onboarding_account_setup/components/add_account_bottom_container.dart';
-import 'package:expense_tracker/presentation/widgets/buttons/buttons.dart';
 import 'package:flutter/material.dart';
 
 /// {@template onboarding_account_setup_body}
@@ -10,9 +8,24 @@ import 'package:flutter/material.dart';
 ///
 /// Add what it does
 /// {@endtemplate}
-class OnboardingAccountSetupBody extends StatelessWidget {
+class OnboardingAccountSetupBody extends StatefulWidget {
   /// {@macro onboarding_account_setup_body}
   const OnboardingAccountSetupBody({super.key});
+
+  @override
+  State<OnboardingAccountSetupBody> createState() =>
+      _OnboardingAccountSetupBodyState();
+}
+
+class _OnboardingAccountSetupBodyState
+    extends State<OnboardingAccountSetupBody> {
+  final TextEditingController _accountBalanceController =
+      TextEditingController();
+  @override
+  void dispose() {
+    _accountBalanceController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -31,15 +44,58 @@ class OnboardingAccountSetupBody extends StatelessWidget {
                 ),
               ),
             ),
+            // Padding(
+            //   padding: const EdgeInsets.symmetric(
+            //     horizontal: 10,
+            //     vertical: 5,
+            //   ),
+            //   child: Text(
+            //     '\$${state.createAccount.acBalance}',
+            //     style: ExpenseTrackerTextStyle.titleX
+            //         .copyWith(color: Colors.white),
+            //   ),
+            // ),
             Padding(
-              padding: const EdgeInsets.symmetric(
-                horizontal: 10,
-                vertical: 5,
-              ),
-              child: Text(
-                '\$${state.createAccount.acBalance}',
-                style: ExpenseTrackerTextStyle.titleX
-                    .copyWith(color: Colors.white),
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: TextField(
+                controller: _accountBalanceController,
+                keyboardType: TextInputType.number,
+                style: ExpenseTrackerTextStyle.titleX.copyWith(
+                  color: ExpenseTrackerColors.light80,
+                ),
+                onSubmitted: (value) {
+                  context.read<OnboardingAccountSetupBloc>().add(
+                        AddBalanceEvent(
+                          double.parse(_accountBalanceController.text),
+                        ),
+                      );
+                },
+                onEditingComplete: () {
+                  context.read<OnboardingAccountSetupBloc>().add(
+                        AddBalanceEvent(
+                          double.parse(_accountBalanceController.text),
+                        ),
+                      );
+                },
+                onChanged: (value) {
+                  context.read<OnboardingAccountSetupBloc>().add(
+                        AddBalanceEvent(
+                          double.parse(_accountBalanceController.text),
+                        ),
+                      );
+                },
+                decoration: InputDecoration(
+                  hintText: '0.00',
+                  prefixIcon: const Icon(
+                    Icons.attach_money,
+                    color: ExpenseTrackerColors.light80,
+                    size: 64,
+                  ),
+                  hintStyle: ExpenseTrackerTextStyle.titleX.copyWith(
+                    color: ExpenseTrackerColors.light80,
+                  ),
+                  border: InputBorder.none,
+                ),
               ),
             ),
             const AddAccountBottomContainer(),
