8d9df4d0e5fc8988ee5ee9a0b530060fb766590d
RomjanHossain <romjan@skiff.com>
2023-12-30 00:00:28 +0600

    Add reverse mapping for mobile banking icons

--- lib/data/datasources/local/utils_data/mobile_banking_db.dart
+++ lib/data/datasources/local/utils_data/mobile_banking_db.dart
@@ -11,3 +11,15 @@ Map<String, String> get mobileBanking => {
       'Ok': ExpenseAssets.okIcon,
       'Tcash': ExpenseAssets.tCashIcon,
     };
+
+Map<String, String> get mobileBankingReverse => {
+      ExpenseAssets.bkashIcon: 'Bkash',
+      ExpenseAssets.nagadIcon: 'Nagad',
+      ExpenseAssets.rocketIcon: 'Rocket',
+      ExpenseAssets.mCashIcon: 'Mkash',
+      ExpenseAssets.sureCashIcon: 'SureCash',
+      ExpenseAssets.ucashIcon: 'Ucash',
+      ExpenseAssets.upayIcon: 'Upay',
+      ExpenseAssets.okIcon: 'Ok',
+      ExpenseAssets.tCashIcon: 'Tcash',
+    };
