8c2d8c71bf30533355abf893fec471a672521792
RomjanHossain <romjan@skiff.com>
2024-01-01 22:56:36 +0600

    Add expense graph to homepage

--- lib/presentation/pages/homepage/view/homepage_page.dart
+++ lib/presentation/pages/homepage/view/homepage_page.dart
@@ -6,6 +6,7 @@ import 'package:expense_tracker/presentation/pages/homepage/components/dropdown_
 import 'package:expense_tracker/presentation/pages/homepage/components/ie_small_card.dart';
 import 'package:expense_tracker/presentation/pages/homepage/widgets/homepage_body.dart';
 import 'package:expense_tracker/utils/constrants/consts_.dart';
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/svg.dart';
@@ -145,6 +146,83 @@ class _HomepagePageState extends State<HomepagePage> {
               //   ),
               // ),
             ),
+            // a graph for showing todays expense
+            SliverToBoxAdapter(
+              child: SizedBox(
+                height: 240.h,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(15).h,
+                      child: Text(
+                        'Spend Frequency',
+                        style: ExpenseTrackerTextStyle.title3.copyWith(
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: AspectRatio(
+                        aspectRatio: 1.7,
+                        child: Container(
+                          decoration: const BoxDecoration(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(18),
+                            ),
+                            // color: Color(0xff232d37),
+                          ),
+                          child: LineChart(
+                            LineChartData(
+                              gridData: const FlGridData(
+                                show: false,
+                              ),
+                              borderData: FlBorderData(
+                                show: false,
+                              ),
+                              titlesData: const FlTitlesData(
+                                show: false,
+                              ),
+                              lineBarsData: [
+                                LineChartBarData(
+                                  spots: const [
+                                    FlSpot(0, 3),
+                                    FlSpot(2.6, 2),
+                                    FlSpot(4.9, 5),
+                                    FlSpot(6.8, 3.1),
+                                    FlSpot(8, 4),
+                                    FlSpot(9.5, 3),
+                                    FlSpot(11, 4),
+                                  ],
+                                  color: ExpenseTrackerColors.violet,
+                                  isCurved: true,
+                                  barWidth: 6.h,
+                                  isStrokeCapRound: true,
+                                  dotData: const FlDotData(
+                                    show: false,
+                                  ),
+                                  belowBarData: BarAreaData(
+                                    show: true,
+                                    gradient: LinearGradient(
+                                      begin: Alignment.topCenter,
+                                      end: Alignment.bottomCenter,
+                                      colors: [
+                                        Color(0xffCA50FF).withOpacity(0.24),
+                                        Color(0xff8B50FF).withOpacity(0),
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
             const SliverToBoxAdapter(
               child: HomepageView(),
             ),
@@ -156,14 +234,13 @@ class _HomepagePageState extends State<HomepagePage> {
                   title: Text('${calanderMonths[now.month - 1]} data $index'),
                 );
               },
-            )
+            ),
           ],
         ),
       ),
     );
   }
 }
-
 
 /// {@template homepage_view}
 /// Displays the Body of HomepageView
