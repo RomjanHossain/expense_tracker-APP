6cc0b4bf1cf129cea54457a0a374a2cb64061514
RomjanHossain <romjan@skiff.com>
2024-01-07 18:31:10 +0600

    Add ExpenseFormEntity class

---
+++ lib/domain/entities/expense_form_entity/expense_form_entity.dart
@@ -0,0 +1,16 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+part 'expense_form_entity.freezed.dart';
+
+@freezed
+class ExpenseFormEntity with _$ExpenseFormEntity {
+  const factory ExpenseFormEntity({
+    required bool isExpense, /// subscription start date
+    String? subStartMonth,
+    String? subStartDay,
+
+    /// subscription end date
+    DateTime? subEnd,
+    // sub type
+    String? subType,
+  }) = _ExpenseFormEntity;
+}
