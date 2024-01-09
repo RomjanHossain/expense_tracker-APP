53b78252020f368f5e577a7a868622803ddbf5c7
RomjanHossain <romjan@skiff.com>
2024-01-10 00:01:55 +0600

    Add dropdownInputDecoration method to utils.dart

--- lib/utils/utils_.dart
+++ lib/utils/utils_.dart
@@ -1,4 +1,5 @@
 import 'package:expense_tracker/app/ui/src/colors.dart';
+import 'package:expense_tracker/app/ui/src/typography/text_styles.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 
@@ -15,3 +16,33 @@ void showToast(String msg, Color? color) {
     fontSize: 16,
   );
 }
+
+  InputDecoration dropdownInputDecoration(String hintText) => InputDecoration(
+        isDense: true,
+        focusedBorder: const OutlineInputBorder(
+          borderRadius: BorderRadius.all(Radius.circular(20)),
+          borderSide: BorderSide(
+            color: ExpenseTrackerColors.violet,
+          ),
+        ),
+        enabledBorder: const OutlineInputBorder(
+          borderRadius: BorderRadius.all(Radius.circular(20)),
+          borderSide: BorderSide(
+            color: ExpenseTrackerColors.light60,
+          ),
+        ),
+        border: const OutlineInputBorder(
+          borderRadius: BorderRadius.all(Radius.circular(15)),
+          borderSide: BorderSide(
+            color: ExpenseTrackerColors.light60,
+          ),
+        ),
+        hintText: hintText,
+        hintStyle: ExpenseTrackerTextStyle.regular2.copyWith(
+          color: ExpenseTrackerColors.light20,
+        ),
+        contentPadding: const EdgeInsets.symmetric(
+          horizontal: 20,
+          vertical: 10,
+        ),
+      );
