0c5c959c892590f82415bb780e7c8a139ba8e423
RomjanHossain <romjan@skiff.com>
2024-01-14 23:54:48 +0600

    Update test cases and add new files for Createbudget feature

---
+++ test/createbudget/cubit/createbudget_cubit_test.dart
@@ -0,0 +1,30 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:bloc_test/bloc_test.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:expense_tracker/presentation/pages/createbudget/cubit/cubit.dart';
+
+void main() {
+  group('CreatebudgetCubit', () {
+    group('constructor', () {
+      test('can be instantiated', () {
+        expect(
+          CreatebudgetCubit(),
+          isNotNull,
+        );
+      });
+    });
+
+    test('initial state has default value for customProperty', () {
+      final createbudgetCubit = CreatebudgetCubit();
+      expect(createbudgetCubit.state.customProperty, equals('Default Value'));
+    });
+
+    blocTest<CreatebudgetCubit, CreatebudgetState>(
+      'yourCustomFunction emits nothing',
+      build: CreatebudgetCubit.new,
+      act: (cubit) => cubit.yourCustomFunction(),
+      expect: () => <CreatebudgetState>[],
+    );
+  });
+}
---
+++ test/createbudget/cubit/createbudget_state_test.dart
@@ -0,0 +1,63 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:expense_tracker/presentation/pages/createbudget/cubit/cubit.dart';
+
+void main() {
+  group('CreatebudgetState', () {
+    test('supports value equality', () {
+      expect(
+        CreatebudgetState(),
+        equals(
+          const CreatebudgetState(),
+        ),
+      );
+    });
+
+    group('constructor', () {
+      test('can be instantiated', () {
+        expect(
+          const CreatebudgetState(),
+          isNotNull,
+        );
+      });
+    });
+
+    group('copyWith', () {
+      test(
+        'copies correctly '
+        'when no argument specified',
+        () {
+          const createbudgetState = CreatebudgetState(
+            customProperty: 'My property',
+          );
+          expect(
+            createbudgetState.copyWith(),
+            equals(createbudgetState),
+          );
+        },
+      );
+
+      test(
+        'copies correctly '
+        'when all arguments specified',
+        () {
+          const createbudgetState = CreatebudgetState(
+            customProperty: 'My property',
+          );
+          final otherCreatebudgetState = CreatebudgetState(
+            customProperty: 'My property 2',
+          );
+          expect(createbudgetState, isNot(equals(otherCreatebudgetState)));
+
+          expect(
+            createbudgetState.copyWith(
+              customProperty: otherCreatebudgetState.customProperty,
+            ),
+            equals(otherCreatebudgetState),
+          );
+        },
+      );
+    });
+  });
+}
