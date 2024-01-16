e0035a288486b09940f5669d93d0476db2c014f1
RomjanHossain <romjan@skiff.com>
2024-01-16 17:38:59 +0600

    Add CurrencyPage to account_page

---
+++ lib/presentation/pages/account_page/pages/currency/currency_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class CurrencyPage extends StatelessWidget {
+  const CurrencyPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Currency"),
+      ),
+    );
+  }
+}
