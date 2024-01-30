20fab5107d02df0af391743cd3acd0278f4d3c67
RomjanHossain <romjan@skiff.com>
2024-01-30 11:52:48 +0600

    Add HelpPage and related widgets

---
+++ lib/presentation/pages/settings/pages/help/view/help_page.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:expense_tracker/presentation/pages/settings/pages/help/widgets/help_body.dart';
+
+/// {@template help_page}
+/// A description for HelpPage
+/// {@endtemplate}
+class HelpPage extends StatelessWidget {
+  /// {@macro help_page}
+  const HelpPage({super.key});
+
+  /// The static route for HelpPage
+  static Route<dynamic> route() {
+    return MaterialPageRoute<dynamic>(builder: (_) => const HelpPage());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Help'),
+      ),
+      body: const HelpView(),
+    );
+  }
+}
+
+/// {@template help_view}
+/// Displays the Body of HelpView
+/// {@endtemplate}
+class HelpView extends StatelessWidget {
+  /// {@macro help_view}
+  const HelpView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const HelpBody();
+  }
+}
---
+++ test/help/view/widgets/help_body_test.dart
@@ -0,0 +1,17 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:expense_tracker/presentation/pages/settings/pages/help/help.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('HelpBody', () {
+    testWidgets('renders Text', (tester) async { 
+      await tester.pumpWidget(
+        MaterialApp(home: HelpBody()),
+      );
+
+      expect(find.byType(Text), findsOneWidget);
+    });
+  });
+}
