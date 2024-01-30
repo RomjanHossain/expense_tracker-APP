b0b0c93456bdec6c5f75b4df802253802236f809
RomjanHossain <romjan@skiff.com>
2024-01-30 15:56:08 +0600

    Refactor notification body widget

--- lib/presentation/pages/settings/pages/notification/widgets/notification_body.dart
+++ lib/presentation/pages/settings/pages/notification/widgets/notification_body.dart
@@ -1,5 +1,7 @@
-import 'package:flutter/material.dart';
+import 'package:expense_tracker/app/ui/app_ui.dart';
 import 'package:expense_tracker/presentation/pages/settings/pages/notification/cubit/cubit.dart';
+import 'package:expense_tracker/utils/utils_.dart';
+import 'package:flutter/material.dart';
 
 /// {@template notification_body}
 /// Body of the NotificationPage.
@@ -14,7 +16,63 @@ class NotificationBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocBuilder<NotificationCubit, NotificationState>(
       builder: (context, state) {
-        return Center(child: Text(state.customProperty));
+        final tileTitle = ExpenseTrackerTextStyle.body1.copyWith(
+          color: isDarkMode(context)
+              ? ExpenseTrackerColors.light80
+              : ExpenseTrackerColors.dark25,
+        );
+        final tileSubtitle = ExpenseTrackerTextStyle.small.copyWith(
+          color: ExpenseTrackerColors.light20,
+        );
+        return Column(
+          children: [
+            ListTile(
+              title: Text(
+                'Expense Alert',
+                style: tileTitle,
+              ),
+              subtitle: Text(
+                'Get notification about you’reexpense',
+                style: tileSubtitle,
+              ),
+              isThreeLine: true,
+              trailing: Switch(
+                  value: state.expenseAlert,
+                  onChanged: (v) {
+                    context.read<NotificationCubit>().setExpenseAlert(v);
+                  }),
+            ),
+            ListTile(
+              title: Text(
+                'Budget',
+                style: tileTitle,
+              ),
+              subtitle: Text(
+                'Get notification when you’re budget exceeding the limit',
+                style: tileSubtitle,
+              ),
+              isThreeLine: true,
+              trailing: Switch(
+                  value: state.budgetAlert,
+                  onChanged: (v) {
+                    context.read<NotificationCubit>().setBudgetAlert(v);
+                  }),
+            ),
+            ListTile(
+              title: Text('Tips & Articles', style: tileTitle),
+              subtitle: Text(
+                'Small & useful pieces of pratical financial advice',
+                style: tileSubtitle,
+              ),
+              isThreeLine: true,
+              trailing: Switch(
+                  value: state.tipsAlert,
+                  onChanged: (v) {
+                    context.read<NotificationCubit>().setTipsAlert(v);
+                  }),
+            ),
+          ],
+        );
       },
     );
   }
