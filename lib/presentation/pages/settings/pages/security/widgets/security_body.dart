1abadfc69a2786773d740d74523a73437c888c0f
RomjanHossain <romjan@skiff.com>
2024-01-30 15:58:16 +0600

    Add imports for dart:io, package:expense_tracker/presentation/pages/settings/pages/theme/components/checkmark_circle.dart, and package:local_auth/local_auth.dart. Update the onTap function for the ListTile with title 'Fingerprint' to show a snackbar based on device support for fingerprint authentication. Update the trailing widget for the ListTile with title 'PIN' to show a CheckMark based on the state of securityPin. closes #54 closes #51

--- lib/presentation/pages/settings/pages/security/widgets/security_body.dart
+++ lib/presentation/pages/settings/pages/security/widgets/security_body.dart
@@ -1,6 +1,10 @@
+import 'dart:io';
+
 import 'package:expense_tracker/presentation/pages/settings/bloc/bloc.dart';
 import 'package:expense_tracker/presentation/pages/settings/pages/security/cubit/security_cubit.dart';
+import 'package:expense_tracker/presentation/pages/settings/pages/theme/components/checkmark_circle.dart';
 import 'package:flutter/material.dart';
+import 'package:local_auth/local_auth.dart';
 
 /// {@template security_body}
 /// Body of the SecurityPage.
@@ -22,14 +26,31 @@ class SecurityBody extends StatelessWidget {
                 // context.read<CurrencyCubit>().useDollarCurrency();
               },
               title: const Text('PIN'),
-              trailing: state is CurrencyDollar ? const CheckMark() : null,
+              trailing: state.securityPin ? const CheckMark() : null,
             ),
             ListTile(
-              onTap: () {
+              onTap: () async {
+                var canAuthenticate = false;
+                if (Platform.isAndroid) {
+                  final auth = LocalAuthentication();
+                  final canAuthenticateWithBiometrics =
+                      await auth.canCheckBiometrics;
+                  canAuthenticate = canAuthenticateWithBiometrics ||
+                      await auth.isDeviceSupported();
+                }
                 // context.read<CurrencyCubit>().useDollarCurrency();
+                // show a snackbar
+                if (!context.mounted) return;
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                    content: Text(canAuthenticate
+                        ? 'Fingerprint feature is not available right now'
+                        : 'Fingerprint is not available'),
+                  ),
+                );
               },
               title: const Text('Fingerprint'),
-              // trailing: state is CurrencyDollar ? const CheckMark() : null,
+              trailing: state.securityFingerprint ? const CheckMark() : null,
             ),
           ],
         );
