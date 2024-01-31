2b0d33733b46bfc56b5ac88cecd1688c51cd738c
RomjanHossain <romjan@skiff.com>
2024-01-31 23:56:51 +0600

    Refactor loginOrGoHome method in SplashService

--- lib/services/splash/splash_service.dart
+++ lib/services/splash/splash_service.dart
@@ -1,36 +1,25 @@
+import 'package:expense_tracker/data/datasources/local/shared_pref/settings_data.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 class SplashService {
   Future<void> loginOrGoHome(BuildContext context) async {
-    final prefs = await SharedPreferences.getInstance();
-    final keepLogin = prefs.getBool('keepLoggedIn');
-    final firstRun = prefs.getBool('firstRun');
+    debugPrint('Login or go home');
+    final prefs = SettingsLocalDataSourcePref();
+    final firstRun = await prefs.isFirstRun();
 
-    if (firstRun == null) {
+    if (firstRun) {
+      debugPrint('First run');
       Future.delayed(const Duration(seconds: 3), () {
         context.pushNamed('onboarding');
       });
-    } else if (keepLogin == false) {
-      Future.delayed(const Duration(seconds: 2), () {
-        // Navigator.pushReplacement<void, void>(
-        //   context,
-        //   MaterialPageRoute<void>(
-        //     builder: (BuildContext context) => const MyHomePage(),
-        //   ),
-        // );
-      });
     } else {
-      final pass = prefs.getString('pin');
+      debugPrint('Not first run');
       //Whether login failed or succeded, go to home page
       if (!context.mounted) return;
-      // await Navigator.pushReplacement<void, void>(
-      //   context,
-      //   MaterialPageRoute<void>(
-      //     builder: (BuildContext context) => const MyHomePage(),
-      //   ),
-      // );
+      Future.delayed(const Duration(seconds: 3), () {
+        context.pushNamed('setup-pin');
+      });
     }
   }
 }
