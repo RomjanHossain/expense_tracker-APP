97dcdb9412ebcfd388a8567dbbce6f6d0505b0c0
RomjanHossain <romjan@skiff.com>
2024-02-18 23:03:51 +0600

    commented out the primary and secondary btn. they don't have any usecases anymore

--- lib/presentation/widgets/buttons/buttons.dart
+++ lib/presentation/widgets/buttons/buttons.dart
@@ -1,45 +1,30 @@
-import 'package:expense_tracker/app/ui/app_ui.dart';
-import 'package:flutter/material.dart';
+// import 'package:expense_tracker/app/ui/app_ui.dart';
+// import 'package:flutter/material.dart';
 
-/// primary button
-class PrimaryButton extends StatelessWidget {
-  const PrimaryButton({required this.onPress, required this.text, super.key});
-  final String text;
-  final void Function() onPress;
-  @override
-  Widget build(BuildContext context) {
-    return ElevatedButton(
-      style: ElevatedButton.styleFrom(
-        minimumSize: const Size(double.infinity, 48),
-        foregroundColor: ExpenseTrackerColors.light,
-        backgroundColor: ExpenseTrackerColors.violet,
-        shape: const RoundedRectangleBorder(
-          borderRadius: ExpenseTrackerTheme.borderRadiusSmall,
-        ),
-      ),
-      onPressed: onPress,
-      child: Text(text),
-    );
-  }
-}
+// /// primary button
+// class ElevatedButton extends StatelessWidget {
+//   const ElevatedButton({required this.onPress, required this.text, super.key});
+//   final String text;
+//   final void Function() onPress;
+//   @override
+//   Widget build(BuildContext context) {
+//     return ElevatedButton(
+//       onPressed: onPress,
+//       child: Text(text),
+//     );
+//   }
+// }
 
-// secondary button
-class SecondaryButton extends StatelessWidget {
-  const SecondaryButton({required this.onPress, required this.text, super.key});
-  final String text;
-  final void Function() onPress;
-  @override
-  Widget build(BuildContext context) {
-    return OutlinedButton(
-      style: OutlinedButton.styleFrom(
-        foregroundColor: ExpenseTrackerColors.violet,
-        backgroundColor: ExpenseTrackerColors.violet20,
-        shape: const RoundedRectangleBorder(
-          borderRadius: ExpenseTrackerTheme.borderRadiusSmall,
-        ),
-      ),
-      onPressed: onPress,
-      child: Text(text),
-    );
-  }
-}
+// // secondary button
+// class OutlinedButton extends StatelessWidget {
+//   const OutlinedButton({required this.onPress, required this.text, super.key});
+//   final String text;
+//   final void Function() onPress;
+//   @override
+//   Widget build(BuildContext context) {
+//     return OutlinedButton(
+//       onPressed: onPress,
+//       child: Text(text),
+//     );
+//   }
+// }
