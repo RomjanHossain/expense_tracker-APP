aceb44540bdc9b7c2615a0413d7d662a426b9f3f
RomjanHossain <romjan@skiff.com>
2024-02-26 15:27:06 +0600

    changed the toast (failor/success)

--- lib/core/helper/helper_.dart
+++ lib/core/helper/helper_.dart
@@ -1,13 +1,42 @@
 import 'package:expense_tracker/app/ui/src/colors.dart';
 import 'package:flutter/material.dart';
-import 'package:fluttertoast/fluttertoast.dart';
+import 'package:flutter_animate/flutter_animate.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:toastification/toastification.dart';
 
-void showToast(String msg, [Color? color]) {
-  Fluttertoast.showToast(
-    msg: msg,
-    toastLength: Toast.LENGTH_LONG,
-    gravity: ToastGravity.BOTTOM,
-    backgroundColor: color ?? ExpenseTrackerColors.light,
-    textColor: color != null ? ExpenseTrackerColors.light : Colors.black,
+/// show success toast
+ToastificationItem showSuccessToast(BuildContext context, String message) {
+  return toastification.show(
+    context: context,
+    type: ToastificationType.success,
+    style: ToastificationStyle.fillColored,
+    title: const Text('Success'),
+    description: Text(message),
+    autoCloseDuration: 4.seconds,
+    borderRadius: BorderRadius.circular(12.r),
+    boxShadow: lowModeShadow,
+    alignment: Alignment.bottomCenter,
+    pauseOnHover: false,
+    showProgressBar: false,
+    primaryColor: ExpenseTrackerColors.green,
+  );
+}
+
+/// show failure toast
+ToastificationItem showFailureToast(BuildContext context, String message) {
+  return toastification.show(
+    context: context,
+    type: ToastificationType.error,
+    style: ToastificationStyle.fillColored,
+    // borderRadius: 20.r,
+    title: const Text('Failure'),
+    description: Text(message),
+    autoCloseDuration: 4.seconds,
+    borderRadius: BorderRadius.circular(12.r),
+    boxShadow: lowModeShadow,
+    showProgressBar: false,
+    pauseOnHover: false,
+    alignment: Alignment.bottomCenter,
+    primaryColor: ExpenseTrackerColors.red,
   );
 }
