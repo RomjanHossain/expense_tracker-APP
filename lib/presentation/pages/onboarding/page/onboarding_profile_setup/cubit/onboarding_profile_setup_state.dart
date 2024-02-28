7430f7352af2ea609e065729b300f700d91769ed
RomjanHossain <romjan@skiff.com>
2024-02-28 15:14:36 +0600

    some refc

--- lib/presentation/pages/onboarding/page/onboarding_profile_setup/cubit/onboarding_profile_setup_state.dart
+++ lib/presentation/pages/onboarding/page/onboarding_profile_setup/cubit/onboarding_profile_setup_state.dart
@@ -5,29 +5,46 @@ part of 'onboarding_profile_setup_cubit.dart';
 /// {@endtemplate}
 class OnboardingProfileSetupState extends Equatable {
   /// {@macro onboarding_profile_setup}
-  const OnboardingProfileSetupState({
-    this.customProperty = 'Default Value',
-  });
+  const OnboardingProfileSetupState();
 
   /// A description for customProperty
-  final String customProperty;
+  // final String customProperty;
 
   @override
-  List<Object> get props => [customProperty];
+  List<Object> get props => [];
 
   /// Creates a copy of the current OnboardingProfileSetupState with property changes
-  OnboardingProfileSetupState copyWith({
-    String? customProperty,
-  }) {
-    return OnboardingProfileSetupState(
-      customProperty: customProperty ?? this.customProperty,
-    );
-  }
+  // OnboardingProfileSetupState copyWith({
+  //   // String? customProperty,
+  // }) {
+  //   return OnboardingProfileSetupState(
+  //     // customProperty: customProperty ?? this.customProperty,
+  //   );
+  // }
 }
+
 /// {@template onboarding_profile_setup_initial}
 /// The initial state of OnboardingProfileSetupState
 /// {@endtemplate}
 class OnboardingProfileSetupInitial extends OnboardingProfileSetupState {
   /// {@macro onboarding_profile_setup_initial}
   const OnboardingProfileSetupInitial() : super();
+}
+
+/// loading state
+class OnboardingProfileSetupLoading extends OnboardingProfileSetupState {
+  /// {@macro onboarding_profile_setup_loading}
+  const OnboardingProfileSetupLoading() : super();
+}
+
+/// success state
+class OnboardingProfileSetupSuccess extends OnboardingProfileSetupState {
+  /// {@macro onboarding_profile_setup_success}
+  const OnboardingProfileSetupSuccess() : super();
+}
+
+/// error state
+class OnboardingProfileSetupError extends OnboardingProfileSetupState {
+  /// {@macro onboarding_profile_setup_error}
+  const OnboardingProfileSetupError() : super();
 }
