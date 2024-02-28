3729d923892991c2c7c718024201eade87c3439f
RomjanHossain <romjan@skiff.com>
2024-02-28 18:07:46 +0600

    save the dataabse and chagnenameEvent

--- lib/presentation/pages/onboarding/page/onboarding_profile_setup/cubit/onboarding_profile_setup_cubit.dart
+++ lib/presentation/pages/onboarding/page/onboarding_profile_setup/cubit/onboarding_profile_setup_cubit.dart
@@ -1,12 +1,24 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:expense_tracker/data/datasources/local/isar_instance.dart';
+import 'package:expense_tracker/data/models/isar_entity/user/user_entity_isar.dart';
 part 'onboarding_profile_setup_state.dart';
 
 class OnboardingProfileSetupCubit extends Cubit<OnboardingProfileSetupState> {
   OnboardingProfileSetupCubit() : super(const OnboardingProfileSetupInitial());
 
+  IsarInstance isar = IsarInstance();
   void saveProfile(String name, String imageURL) {
-    // save profile
+    emit(const OnboardingProfileSetupLoading());
+    final newUser = UserEntity()
+      ..name = name
+      ..imageUrl = imageURL;
+    isar.saveUser(newUser);
     emit(const OnboardingProfileSetupSuccess());
+  }
+
+  // change the name
+  void changeName(String name) {
+    emit(state.copyWith(name: name));
   }
 }
