2db68e69b736ff774454db08d287bc1c41d79d10
RomjanHossain <romjan@skiff.com>
2024-02-28 15:14:42 +0600

    save profile

--- lib/presentation/pages/onboarding/page/onboarding_profile_setup/cubit/onboarding_profile_setup_cubit.dart
+++ lib/presentation/pages/onboarding/page/onboarding_profile_setup/cubit/onboarding_profile_setup_cubit.dart
@@ -2,10 +2,11 @@ import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 part 'onboarding_profile_setup_state.dart';
 
-class OnboardingProfileSetupCubit extends Cubit<String> {
-  OnboardingProfileSetupCubit() : super('');
+class OnboardingProfileSetupCubit extends Cubit<OnboardingProfileSetupState> {
+  OnboardingProfileSetupCubit() : super(const OnboardingProfileSetupInitial());
 
-  void changeName(String value) {
-    emit(value);
+  void saveProfile(String name, String imageURL) {
+    // save profile
+    emit(const OnboardingProfileSetupSuccess());
   }
 }
