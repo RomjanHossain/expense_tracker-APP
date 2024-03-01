b7a2dbc94ed2cd2e6f09e39b04a717c6f672011d
RomjanHossain <romjan@skiff.com>
2024-03-01 23:24:33 +0600

    info toast

--- lib/core/helper/helper_.dart
+++ lib/core/helper/helper_.dart
@@ -5,38 +5,63 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:toastification/toastification.dart';
 
 /// show success toast
-ToastificationItem showSuccessToast(BuildContext context, String message) {
-  return toastification.show(
-    context: context,
-    type: ToastificationType.success,
-    style: ToastificationStyle.fillColored,
-    title: const Text('Success'),
-    description: Text(message),
-    autoCloseDuration: 4.seconds,
-    borderRadius: BorderRadius.circular(12.r),
-    boxShadow: lowModeShadow,
-    alignment: Alignment.bottomCenter,
-    pauseOnHover: false,
-    showProgressBar: false,
-    primaryColor: ExpenseTrackerColors.green,
-  );
-}
+ToastificationItem showSuccessToast(BuildContext context, String message) =>
+    toastification.show(
+      context: context,
+      type: ToastificationType.success,
+      style: ToastificationStyle.fillColored,
+      title: const Text('Success'),
+      description: Text(message),
+      autoCloseDuration: 4.seconds,
+      borderRadius: BorderRadius.circular(12.r),
+      boxShadow: lowModeShadow,
+      alignment: Alignment.bottomCenter,
+      pauseOnHover: false,
+      showProgressBar: false,
+      primaryColor: ExpenseTrackerColors.green,
+    );
 
 /// show failure toast
-ToastificationItem showFailureToast(BuildContext context, String message) {
-  return toastification.show(
-    context: context,
-    type: ToastificationType.error,
-    style: ToastificationStyle.fillColored,
-    // borderRadius: 20.r,
-    title: const Text('Failure'),
-    description: Text(message),
-    autoCloseDuration: 4.seconds,
-    borderRadius: BorderRadius.circular(12.r),
-    boxShadow: lowModeShadow,
-    showProgressBar: false,
-    pauseOnHover: false,
-    alignment: Alignment.bottomCenter,
-    primaryColor: ExpenseTrackerColors.red,
-  );
-}
+ToastificationItem showFailureToast(BuildContext context, String message) =>
+    toastification.show(
+      context: context,
+      type: ToastificationType.error,
+      style: ToastificationStyle.fillColored,
+      // borderRadius: 20.r,
+      title: const Text('Failure'),
+      description: Text(message),
+      autoCloseDuration: 4.seconds,
+      borderRadius: BorderRadius.circular(12.r),
+      boxShadow: lowModeShadow,
+      showProgressBar: false,
+      pauseOnHover: false,
+      alignment: Alignment.bottomCenter,
+      primaryColor: ExpenseTrackerColors.red,
+    );
+
+// show info
+
+ToastificationItem showInfoToast(BuildContext context, String message) =>
+    toastification.show(
+      context: context,
+      type: ToastificationType.info,
+      style: ToastificationStyle.fillColored,
+      title: const Text('info'),
+      description: Text(message),
+      alignment: Alignment.bottomCenter,
+      autoCloseDuration: 4.seconds,
+      showProgressBar: false,
+      animationBuilder: (
+        context,
+        animation,
+        alignment,
+        child,
+      ) {
+        return ScaleTransition(
+          scale: animation,
+          child: child,
+        );
+      },
+      borderRadius: BorderRadius.circular(12.0),
+      boxShadow: lowModeShadow,
+    );
