48ca822fb9ef9d31c7a26bba3132a7164c24dc3d
RomjanHossain <romjan@skiff.com>
2024-03-02 11:54:38 +0600

    abstact account setup repo

---
+++ lib/data/repositories/account_setup/accoutn_setup_repo.dart
@@ -0,0 +1,14 @@
+import 'dart:async';
+
+import 'package:expense_tracker/data/models/isar_entity/create_account/create_account_isar.dart';
+
+abstract class AccountSetupRepo {
+  //NOTE: create an account
+  Future<int> openAnAccount(AccountEntity accountEntity);
+  // //NOTE: edit an account
+  // Future<void> editAnAccount(int id);
+  //NOTE: delete an account
+  Future<bool> deleteDaAccount(int id);
+  //NOTE: update the account
+  Future<int> updateTheAccount(int id, AccountEntity accountEntity);
+}
