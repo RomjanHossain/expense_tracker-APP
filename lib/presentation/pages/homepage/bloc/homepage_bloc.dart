b9bc8ca972d7c27bcc6ab1b5b8395ad405a70030
RomjanHossain <romjan@skiff.com>
2024-03-04 19:49:59 +0600

    niceeee

--- lib/presentation/pages/homepage/bloc/homepage_bloc.dart
+++ lib/presentation/pages/homepage/bloc/homepage_bloc.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:expense_tracker/utils/constrants/consts_.dart';
 import 'package:expense_tracker/utils/utils_.dart';
 part 'homepage_event.dart';
 part 'homepage_state.dart';
@@ -9,6 +10,7 @@ part 'homepage_state.dart';
 class HomepageBloc extends Bloc<HomepageEvent, HomepageState> {
   HomepageBloc() : super(const HomepageInitial()) {
     on<CustomHomepageEvent>(_onCustomHomepageEvent);
+    on<ChangeTheFreq>(_changetheFreq);
   }
 
   FutureOr<void> _onCustomHomepageEvent(
@@ -16,5 +18,14 @@ class HomepageBloc extends Bloc<HomepageEvent, HomepageState> {
     Emitter<HomepageState> emit,
   ) {
     // TODO: Add Logic
+  }
+  //NOTE: change the freq
+  FutureOr<void> _changetheFreq(
+    ChangeTheFreq event,
+    Emitter<HomepageState> emit,
+  ) {
+    if (event.se != state.se) {
+      emit(state.copyWith(se: event.se));
+    }
   }
 }
