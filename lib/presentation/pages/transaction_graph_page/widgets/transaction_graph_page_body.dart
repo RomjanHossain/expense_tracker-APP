6cd3fa8870aa15b0c3d9bcf607be914c1632196b
RomjanHossain <romjan@skiff.com>
2024-03-06 19:43:53 +0600

    nicely done

--- lib/presentation/pages/transaction_graph_page/widgets/transaction_graph_page_body.dart
+++ lib/presentation/pages/transaction_graph_page/widgets/transaction_graph_page_body.dart
@@ -1,3 +1,4 @@
+import 'package:animations/animations.dart';
 import 'package:expense_tracker/app/ui/app_ui.dart';
 import 'package:expense_tracker/domain/entities/card_of_expense/card_of_expense_entity.dart';
 import 'package:expense_tracker/presentation/pages/expensedetails/view/expensedetails_page.dart';
@@ -23,100 +24,81 @@ class TransactionGraphPageBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<TransactionGraphPageBloc, TransactionGraphPageState>(
-      builder: (context, state) {
-        return Padding(
-          padding: const EdgeInsets.all(10),
-          child: ListView(
-            children: [
-              SizedBox(
-                height: 40.h,
-                child: Row(
-                  children: [
-                    // show dropdown of subscriptionsFrequency
-                    Expanded(
-                      child: DropdownButtonFormField(
-                        isExpanded: true,
-                        dropdownColor: ExpenseTrackerColors.violet,
-                        focusColor: isDarkMode(context)
-                            ? ExpenseTrackerColors.dark75
-                            : ExpenseTrackerColors.light,
-                        decoration: dropdownInputDecoration('Frequency'),
-                        items: SubscriptionsFrequency.values
-                            .map(
-                              (e) => DropdownMenuItem(
-                                value: e,
-                                child: Text(getSucriptionFrequencyText(e)),
-                              ),
-                            )
-                            .toList(),
-                        onChanged: (value) {},
-                      ),
+      builder: (context, state) => Padding(
+        padding: const EdgeInsets.all(10),
+        child: ListView(
+          children: [
+            SizedBox(
+              height: 40.h,
+              child: Row(
+                children: [
+                  //NOTE: show dropdown of subscriptionsFrequency
+                  Expanded(
+                    child: DropdownButtonFormField(
+                      isExpanded: true,
+                      dropdownColor: ExpenseTrackerColors.violet,
+                      focusColor: isDarkMode(context)
+                          ? ExpenseTrackerColors.dark75
+                          : ExpenseTrackerColors.light,
+                      decoration: dropdownInputDecoration('Frequency'),
+                      items: SubscriptionsFrequency.values
+                          .map(
+                            (e) => DropdownMenuItem(
+                              value: e,
+                              child: Text(getSucriptionFrequencyText(e)),
+                            ),
+                          )
+                          .toList(),
+                      onChanged: (value) {},
                     ),
-                    // spacer
-                    const Spacer(),
-                    // a popup menu button to show the list of subscriptions
-                    Badge(
-                      label: const Text(
-                        '3',
-                        style: TextStyle(
-                          color: ExpenseTrackerColors.light,
-                        ),
-                      ),
-                      backgroundColor: ExpenseTrackerColors.violet,
-                      // alignment: Alignment.center,
-                      largeSize: 20.r,
-                      child: IconButton.filled(
-                        onPressed: () async {
-                          // a bottom sheet to
-                          await showModalBottomSheet<void>(
-                            isScrollControlled: true,
-                            backgroundColor:
-                                Theme.of(context).scaffoldBackgroundColor,
-                            context: context,
-                            builder: (context) {
-                              return const TransactionFilterSheet();
-                            },
-                          );
+                  ),
+                  // spacer
+                  const Spacer(),
+                  //NOTE: a popup menu button to show the list of subscriptions
+                  IconButton.filled(
+                    onPressed: () async {
+                      // a bottom sheet to
+                      await showModalBottomSheet<void>(
+                        isScrollControlled: true,
+                        backgroundColor:
+                            Theme.of(context).scaffoldBackgroundColor,
+                        context: context,
+                        builder: (context) {
+                          return const TransactionFilterSheet();
                         },
-                        style: ButtonStyle(
-                          // ractangular shape
-                          shape: MaterialStateProperty.all(
-                            RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(10).r,
-                            ),
-                          ),
-                          // border
-                          side: MaterialStateProperty.all(
-                            const BorderSide(
-                              color: ExpenseTrackerColors.light60,
-                            ),
-                          ),
-                          // backgorund color to trans
-                          backgroundColor:
-                              MaterialStateProperty.all(Colors.transparent),
-                        ),
-                        icon: Icon(
-                          Icons.filter_list,
-                          color: !isDarkMode(context)
-                              ? ExpenseTrackerColors.dark50
-                              : ExpenseTrackerColors.light40,
+                      );
+                    },
+                    style: ButtonStyle(
+                      // ractangular shape
+                      shape: MaterialStateProperty.all(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10).r,
                         ),
                       ),
+                      // border
+                      side: MaterialStateProperty.all(
+                        const BorderSide(
+                          color: ExpenseTrackerColors.light60,
+                        ),
+                      ),
+                      // backgorund color to trans
+                      backgroundColor:
+                          MaterialStateProperty.all(Colors.transparent),
                     ),
-                  ],
-                ),
+                    icon: Icon(
+                      Icons.filter_list,
+                      color: !isDarkMode(context)
+                          ? ExpenseTrackerColors.dark50
+                          : ExpenseTrackerColors.light40,
+                    ),
+                  ),
+                ],
               ),
-              GestureDetector(
-                onTap: () {
-                  //! financial reports
-                  Navigator.push(
-                    context,
-                    PageAnimation.fadeThroughTransitionPageWrapper(
-                      const FinancialReportsQuick(),
-                    ),
-                  );
-                },
-                child: Container(
+            ),
+            //NOTE: goto financial reports
+            OpenContainer(
+              closedBuilder: (context, action) {
+                return Container(
                   padding: const EdgeInsets.symmetric(
                     horizontal: 10,
                     vertical: 10,
@@ -144,47 +126,93 @@ class TransactionGraphPageBody extends StatelessWidget {
                       ),
                     ],
                   ),
-                ),
+                );
+              },
+              openElevation: 0,
+              closedElevation: 0,
+              closedColor: Colors.transparent,
+              openBuilder: (context, action) {
+                return const FinancialReportsQuick();
+              },
+            ),
+            // GestureDetector(
+            //   onTap: () {
+            //     //! financial reports
+            //     Navigator.push(
+            //       context,
+            //       PageAnimation.fadeThroughTransitionPageWrapper(
+            //         const FinancialReportsQuick(),
+            //       ),
+            //     );
+            //   },
+            //   child: Container(
+            //     padding: const EdgeInsets.symmetric(
+            //       horizontal: 10,
+            //       vertical: 10,
+            //     ),
+            //     margin: const EdgeInsets.symmetric(
+            //       // horizontal: 10,
+            //       vertical: 20,
+            //     ),
+            //     decoration: BoxDecoration(
+            //       color: ExpenseTrackerColors.violet20,
+            //       borderRadius: BorderRadius.circular(10).r,
+            //     ),
+            //     child: Row(
+            //       mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            //       children: [
+            //         Text(
+            //           'See your financial reports',
+            //           style: ExpenseTrackerTextStyle.small.copyWith(
+            //             color: ExpenseTrackerColors.violet,
+            //           ),
+            //         ),
+            //         const Icon(
+            //           Icons.navigate_next,
+            //           color: ExpenseTrackerColors.violet,
+            //         ),
+            //       ],
+            //     ),
+            //   ),
+            // ),
+            // today
+            Text(
+              'Today',
+              style: ExpenseTrackerTextStyle.title3.copyWith(
+                fontWeight: FontWeight.w600,
+                letterSpacing: 0.5,
+                color: !isDarkMode(context)
+                    ? ExpenseTrackerColors.dark
+                    : ExpenseTrackerColors.light,
               ),
-              // today
-              Text(
-                'Today',
-                style: ExpenseTrackerTextStyle.title3.copyWith(
-                  fontWeight: FontWeight.w600,
-                  letterSpacing: 0.5,
-                  color: !isDarkMode(context)
-                      ? ExpenseTrackerColors.dark
-                      : ExpenseTrackerColors.light,
-                ),
-              ),
-              ListView.builder(
-                shrinkWrap: true,
-                itemCount: 5,
-                physics: const NeverScrollableScrollPhysics(),
-                itemBuilder: (context, index) {
-                  return GestureDetector(
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        ExpensedetailsPage.route(ExpenseTrackerColors.red),
-                      );
-                    },
-                    child: const CardOfExpense(
-                      cardOfExpense: CardOfExpenseEntity(
-                        color: ExpenseTrackerColors.violet,
-                        title: 'householdRepairs',
-                        subtitle: 'Walmart',
-                        amount: 23,
-                        date: 'Today',
-                      ),
+            ),
+            ListView.builder(
+              shrinkWrap: true,
+              itemCount: 5,
+              physics: const NeverScrollableScrollPhysics(),
+              itemBuilder: (context, index) {
+                return GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      ExpensedetailsPage.route(ExpenseTrackerColors.red),
+                    );
+                  },
+                  child: const CardOfExpense(
+                    cardOfExpense: CardOfExpenseEntity(
+                      color: ExpenseTrackerColors.violet,
+                      title: 'householdRepairs',
+                      subtitle: 'Walmart',
+                      amount: 23,
+                      date: 'Today',
                     ),
-                  );
-                },
-              ),
-            ],
-          ),
-        );
-      },
+                  ),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
