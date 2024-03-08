7635ae00ac67a49f39c5d14a8d0da0710a0dd45d
RomjanHossain <romjan@skiff.com>
2024-03-08 16:40:33 +0600

    drodpwn accoutn cubit

---
+++ lib/presentation/cubit/dropdown_data/dropdown_account_cubit.dart
@@ -0,0 +1,19 @@
+import 'package:expense_tracker/data/datasources/local/isar_instance.dart';
+import 'package:expense_tracker/data/models/isar_entity/create_account/create_account_isar.dart';
+import 'package:expense_tracker/presentation/pages/profile_page/bloc/bloc.dart';
+
+class DropdownAccountCubit
+    extends Cubit<(List<AccountEntity>, AccountEntity?)> {
+  DropdownAccountCubit() : super(([], null));
+
+  Future<void> runOnFirst() async {
+    final isarI = IsarInstance();
+    final listOfAc = await isarI.getListOfAccount();
+    emit((listOfAc, null));
+  }
+
+  /// chagne the selected account
+  void changeSelectedAccount(AccountEntity? account) {
+    emit((state.$1, account));
+  }
+}
