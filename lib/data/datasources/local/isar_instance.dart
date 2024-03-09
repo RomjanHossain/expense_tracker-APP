1404436b5ba0e4f932fa993f9f5ea6a9bd4e9ac6
RomjanHossain <romjan@skiff.com>
2024-03-10 00:22:20 +0600

    IncomeRepo, ExpenseRepo, TransferRepo IsarInstance

--- lib/data/datasources/local/isar_instance.dart
+++ lib/data/datasources/local/isar_instance.dart
@@ -1,6 +1,12 @@
 import 'package:expense_tracker/data/models/isar_entity/create_account/create_account_isar.dart';
+import 'package:expense_tracker/data/models/isar_entity/expense_entity/expense_entity.dart';
+import 'package:expense_tracker/data/models/isar_entity/income_entity/income_entity.dart';
+import 'package:expense_tracker/data/models/isar_entity/transfer_entity/transfer_entity.dart';
 import 'package:expense_tracker/data/models/isar_entity/user/user_entity_isar.dart';
 import 'package:expense_tracker/data/repositories/account_setup/accoutn_setup_repo.dart';
+import 'package:expense_tracker/data/repositories/expense_repo/expense_repo.dart';
+import 'package:expense_tracker/data/repositories/income_repo/income_repo.dart';
+import 'package:expense_tracker/data/repositories/transfer_repo/transfer_repo.dart';
 import 'package:expense_tracker/data/repositories/user/user_repo.dart';
 import 'package:flutter/rendering.dart';
 import 'package:isar/isar.dart';
@@ -11,7 +17,14 @@ part 'iisar_instance.dart';
 /// {@template isar_instance}
 /// IsarInstance description
 /// {@endtemplate}
-class IsarInstance implements IIsarInstance, UserRepository, AccountSetupRepo {
+class IsarInstance
+    implements
+        IIsarInstance,
+        UserRepository,
+        AccountSetupRepo,
+        IncomeRepo,
+        ExpenseRepo,
+        TransferRepo {
   /// {@macro isar_instance}
   factory IsarInstance() {
     return _instance;
@@ -31,6 +44,10 @@ class IsarInstance implements IIsarInstance, UserRepository, AccountSetupRepo {
         [
           UserEntitySchema,
           AccountEntitySchema,
+          // For income, expense and transfer
+          IncomeIsarEntitySchema,
+          ExpenseIsarEntitySchema,
+          TransferEntitySchema,
         ],
         directory: dir.path,
       );
@@ -123,5 +140,123 @@ class IsarInstance implements IIsarInstance, UserRepository, AccountSetupRepo {
   Future<List<AccountEntity>> getListOfAccount() async {
     final ins = await instance;
     return ins.accountEntitys.where().findAll();
+  }
+
+  //WARN: expense section
+  @override
+  Future<int> createAnExpense(ExpenseIsarEntity expenseEntity) async {
+    final ins = await instance;
+    return ins.writeTxn(() => ins.expenseIsarEntitys.put(expenseEntity));
+  }
+
+  @override
+  Future<List<ExpenseIsarEntity>> getListOfExpense() async {
+    final ins = await instance;
+    return ins.expenseIsarEntitys.where().findAll();
+  }
+
+  @override
+  Future<int> updateTheExpense(int id, ExpenseIsarEntity expenseEntity) async {
+    final ins = await instance;
+    final currentExpense = await ins.expenseIsarEntitys.get(id);
+    if (currentExpense != null) {
+      currentExpense
+        ..ammount = expenseEntity.ammount
+        ..createdDate = expenseEntity.createdDate
+        ..walletId = expenseEntity.walletId
+        ..repeatType = expenseEntity.repeatType
+        ..startDate = expenseEntity.startDate
+        ..endDate = expenseEntity.endDate
+        ..isRepeat = expenseEntity.isRepeat
+        ..attachment = expenseEntity.attachment
+        ..description = expenseEntity.description;
+      return ins.writeTxn(() => ins.expenseIsarEntitys.put(currentExpense));
+    } else {
+      return 0;
+    }
+  }
+
+  @override
+  Future<bool> deleteAnExpense(int id) async {
+    final ins = await instance;
+    return ins.writeTxn(() => ins.expenseIsarEntitys.delete(id));
+  }
+
+  //WARN: income section
+  @override
+  Future<int> updateTheIncome(int id, IncomeIsarEntity incomeEntity) async {
+    final ins = await instance;
+    final currentIncome = await ins.incomeIsarEntitys.get(id);
+
+    if (currentIncome != null) {
+      currentIncome
+        ..ammount = incomeEntity.ammount
+        ..createdDate = incomeEntity.createdDate
+        ..walletId = incomeEntity.walletId
+        ..repeatType = incomeEntity.repeatType
+        ..startDate = incomeEntity.startDate
+        ..endDate = incomeEntity.endDate
+        ..isRepeat = incomeEntity.isRepeat
+        ..attachment = incomeEntity.attachment
+        ..description = incomeEntity.description;
+      return ins.writeTxn(() => ins.incomeIsarEntitys.put(currentIncome));
+    } else {
+      return 0;
+    }
+  }
+
+  @override
+  Future<int> createAnIncome(IncomeIsarEntity incomeEntity) async {
+    final ins = await instance;
+    return ins.writeTxn(() => ins.incomeIsarEntitys.put(incomeEntity));
+  }
+
+  @override
+  Future<List<IncomeIsarEntity>> getListOfIncome() async {
+    final ins = await instance;
+    return ins.incomeIsarEntitys.where().findAll();
+  }
+
+  @override
+  Future<bool> deleteAnIncome(int id) async {
+    final ins = await instance;
+    return ins.writeTxn(() => ins.incomeIsarEntitys.delete(id));
+  }
+
+//WARN: transfer section
+  @override
+  Future<int> createAnTransfer(TransferEntity transferEntity) async {
+    final ins = await instance;
+    return ins.writeTxn(() => ins.transferEntitys.put(transferEntity));
+  }
+
+  @override
+  Future<bool> deleteAnTransfer(int id) async {
+    final ins = await instance;
+    return ins.writeTxn(() => ins.transferEntitys.delete(id));
+  }
+
+  @override
+  Future<List<TransferEntity>> getListOfTransfer() async {
+    final ins = await instance;
+    return ins.transferEntitys.where().findAll();
+  }
+
+  @override
+  Future<int> updateTheTransfer(int id, TransferEntity transferEntity) async {
+    final ins = await instance;
+    final currentTransfer = await ins.transferEntitys.get(id);
+    if (currentTransfer != null) {
+      currentTransfer
+        ..ammount = transferEntity.ammount
+        ..createdDate = transferEntity.createdDate
+        ..attachment = transferEntity.attachment
+        ..description = transferEntity.description
+        ..fromID = transferEntity.fromID
+        ..to = transferEntity.to;
+      return ins.writeTxn(() => ins.transferEntitys.put(currentTransfer));
+    } else {
+      return 0;
+    }
   }
 }
