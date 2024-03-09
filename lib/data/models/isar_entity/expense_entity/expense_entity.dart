9a736a39ab47841e7822dcaeaf505e21f95a739e
RomjanHossain <romjan@skiff.com>
2024-03-10 00:23:08 +0600

    expense isar entity

---
+++ lib/data/models/isar_entity/expense_entity/expense_entity.dart
@@ -0,0 +1,17 @@
+import 'package:isar/isar.dart';
+part 'expense_entity.g.dart';
+
+@collection
+class ExpenseIsarEntity {
+  @Index()
+  Id id = Isar.autoIncrement;
+  double? ammount;
+  String? description;
+  int? walletId;
+  String? attachment;
+  bool? isRepeat;
+  String? repeatType;
+  DateTime? startDate;
+  DateTime? endDate;
+  DateTime? createdDate;
+}
