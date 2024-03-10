a6cc0bda633b46e2dd14a37203a9c4d894ca1b1f
RomjanHossain <romjan@skiff.com>
2024-03-10 12:55:43 +0600

    success or fail dialog

--- lib/presentation/pages/expenseform/components/success_alertdialog.dart
+++ lib/presentation/pages/expenseform/components/success_alertdialog.dart
@@ -1,41 +1,42 @@
 import 'package:expense_tracker/app/ui/src/colors.dart';
 import 'package:expense_tracker/app/ui/src/typography/text_styles.dart';
+import 'package:expense_tracker/utils/constrants/enums_.dart';
 import 'package:flutter/material.dart';
 
 class SuccessAlertDialog extends StatelessWidget {
   const SuccessAlertDialog({
+    required this.status,
     super.key,
   });
+
+  final ExpenseformStatus status;
 
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
       backgroundColor: ExpenseTrackerColors.light,
       insetPadding: const EdgeInsets.all(10),
-      // title: const Text('Success'),
       content: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
-          const Icon(
-            Icons.check_circle_sharp,
-            color: ExpenseTrackerColors.violet,
-            size: 48,
-          ),
+          if (status == ExpenseformStatus.success)
+            const Icon(
+              Icons.check_circle_sharp,
+              color: ExpenseTrackerColors.violet,
+              size: 48,
+            )
+          else
+            const Icon(
+              Icons.error_sharp,
+              color: ExpenseTrackerColors.red,
+              size: 48,
+            ),
           Text(
             'Transaction has been successfully added',
             style: ExpenseTrackerTextStyle.small,
           ),
         ],
       ),
-      // actions: [
-      //   TextButton(
-      //     onPressed: () {
-      //       // Navigator.of(context).pop();
-      //       // Navigator.of(context).pop();
-      //     },
-      //     child: const Text('Ok'),
-      //   ),
-      // ],
     );
   }
 }
