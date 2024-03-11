832b0d9bf7605ca36e2afc8d699c3f333ace9729
RomjanHossain <romjan@skiff.com>
2024-03-11 09:43:44 +0600

    statefull to blocconsumer

--- lib/presentation/pages/homepage/components/dropdown_months.dart
+++ lib/presentation/pages/homepage/components/dropdown_months.dart
@@ -1,58 +1,62 @@
 import 'package:expense_tracker/app/ui/app_ui.dart';
 import 'package:expense_tracker/app/ui/src/assets/assets_icons_n_illustration.dart';
 import 'package:expense_tracker/data/datasources/local/utils_data/all_months.dart';
+import 'package:expense_tracker/presentation/pages/homepage/bloc/homepage_bloc.dart';
+import 'package:expense_tracker/presentation/pages/profile_page/bloc/bloc.dart';
 import 'package:expense_tracker/utils/utils_.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/svg.dart';
 
-class DropdownMonths extends StatefulWidget {
-  const DropdownMonths({
-    super.key,
-  });
+class DropdownMonths extends StatelessWidget {
+  const DropdownMonths({super.key});
 
-  @override
-  State<DropdownMonths> createState() => _DropdownMonthsState();
-}
-
-class _DropdownMonthsState extends State<DropdownMonths> {
-  String dpValue = calanderMonths[DateTime.now().month - 1];
   @override
   Widget build(BuildContext context) {
-    return DropdownButtonHideUnderline(
-      child: DropdownButton(
-        borderRadius: BorderRadius.circular(20).r,
-        dropdownColor: isDarkMode(context)
-            ? ExpenseTrackerColors.violet60
-            : ExpenseTrackerColors.violet20,
-        alignment: Alignment.center,
-        elevation: 0,
-        style: ExpenseTrackerTextStyle.body3.copyWith(
-          color: ExpenseTrackerColors.dark50,
-        ),
-        icon: SvgPicture.asset(
-          ExpenseAssets.arrowDownIcon,
-          color: ExpenseTrackerColors.violet,
-          height: 20.h,
-        ),
+    return BlocConsumer<HomepageBloc, HomepageState>(
+      builder: (context, state) {
+        return DropdownButtonHideUnderline(
+          child: DropdownButton<String>(
+            borderRadius: BorderRadius.circular(20).r,
+            dropdownColor: isDarkMode(context)
+                ? ExpenseTrackerColors.violet60
+                : ExpenseTrackerColors.violet20,
+            alignment: Alignment.center,
+            elevation: 0,
+            style: ExpenseTrackerTextStyle.body3.copyWith(
+              color: ExpenseTrackerColors.dark50,
+            ),
+            icon: SvgPicture.asset(
+              ExpenseAssets.arrowDownIcon,
+              color: ExpenseTrackerColors.violet,
+              height: 20.h,
+            ),
 
-        value: dpValue,
-        // value: dpValue,
-        // hint: const Text('Select Month'),
-        items: calanderMonths
-            .map(
-              (e) => DropdownMenuItem(
-                value: e,
-                child: Text(e),
-              ),
-            )
-            .toList(),
-        onChanged: (value) {
-          setState(() {
-            dpValue = value.toString();
-          });
-        },
-      ),
+            value: calanderMonths.elementAt(state.currentMonth),
+            // value: dpValue,
+            // hint: const Text('Select Month'),
+            items: calanderMonths
+                .map(
+                  (e) => DropdownMenuItem(
+                    value: e,
+                    child: Text(e),
+                  ),
+                )
+                .toList(),
+            onChanged: (String? value) {
+              // setState(() {
+              //   dpValue = value.toString();
+              // });
+              if (value != null) {
+                context
+                    .read<HomepageBloc>()
+                    .add(ChangeTheMonth(calanderMonths.indexOf(value)));
+              }
+            },
+          ),
+        );
+      },
+      listener: (context, state) {},
     );
   }
 }
