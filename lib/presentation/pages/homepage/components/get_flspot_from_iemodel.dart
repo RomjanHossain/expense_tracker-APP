488b8cb971ebdb0c57b3eb3c26a50ce822c01d56
RomjanHossain <romjan@skiff.com>
2024-03-15 23:42:52 +0600

    some max/min computation

--- lib/presentation/pages/homepage/components/get_flspot_from_iemodel.dart
+++ lib/presentation/pages/homepage/components/get_flspot_from_iemodel.dart
@@ -4,6 +4,73 @@ import 'package:expense_tracker/utils/constrants/extensions_.dart';
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 
+//!INFO: get min amount value from IEmodel
+double getMinAmountValueFromIEmodel(List<IEmodel> models) {
+  final totalEx =
+      models.where((element) => element.isIncome == ExpenseType.expense);
+  final min = totalEx
+      .map((e) => e.expense?.ammount ?? 0)
+      .reduce((value, element) => value < element ? value : element);
+  return min;
+}
+
+//!INFO: get max amount value from IEmodel
+double getMaxAmountValueFromIEmodel(List<IEmodel> models) {
+  final totalEx =
+      models.where((element) => element.isIncome == ExpenseType.expense);
+  final max = totalEx
+      .map((e) => e.expense?.ammount ?? 0)
+      .reduce((value, element) => value > element ? value : element);
+  return max;
+}
+
+//!INFO: get min createdDate value from IEmodel
+double getMinCreatedDateValueFromIEmodel(List<IEmodel> models) {
+  final totalEx =
+      models.where((element) => element.isIncome == ExpenseType.expense);
+  final min = totalEx
+      .map((e) => e.expense!.createdDate!.toDoubleT)
+      .reduce((value, element) => value < element ? value : element);
+  return min;
+}
+
+//!INFO: get max createdDate value from IEmodel
+double getMaxCreatedDateValueFromIEmodel(List<IEmodel> models) {
+  final totalEx =
+      models.where((element) => element.isIncome == ExpenseType.expense);
+  final max = totalEx
+      .map((e) => e.expense!.createdDate!.toDoubleT)
+      .reduce((value, element) => value > element ? value : element);
+  return max;
+}
+
+//INFO: this function is for today
+
+List<FlSpot> getFlSpotFromIEmodelToday(
+  List<IEmodel> models,
+) {
+  final totalEx =
+      models.where((element) => element.isIncome == ExpenseType.expense);
+
+  final spots = <FlSpot>[];
+  // debugPrint('Total Len: ${totalEx.length}');
+  for (final i in totalEx) {
+    final x = i.expense!.createdDate!.toDoubleT;
+    final y = i.expense?.ammount ?? 0;
+    debugPrint(
+      'x: $x, y = $y',
+    );
+    spots.add(
+      FlSpot(
+        x,
+        y,
+      ),
+    );
+  }
+  return spots;
+}
+
+// INFO: This function is used to convert the IEmodel to FlSpot
 List<FlSpot?> getFlSpotFromIEmodel(
   List<IEmodel> models,
 ) {
@@ -11,10 +78,11 @@ List<FlSpot?> getFlSpotFromIEmodel(
       models.where((element) => element.isIncome == ExpenseType.expense);
 
   final spots = <FlSpot>[];
-  debugPrint('Total Len: ${totalEx.length}');
+  // debugPrint('Total Len: ${totalEx.length}');
   for (final i in totalEx) {
     debugPrint(
-        "x: ${i.expense!.createdDate!.toDoubleT}, y = ${i.expense?.ammount ?? 0}");
+      'x: ${i.expense!.createdDate!.toDoubleT}, y = ${i.expense?.ammount ?? 0}',
+    );
     spots.add(
       FlSpot(
         i.expense!.createdDate!.day.toDouble(),
@@ -23,47 +91,4 @@ List<FlSpot?> getFlSpotFromIEmodel(
     );
   }
   return spots;
-  // switch (stype) {
-  //   case SegmentedButtonsData.today:
-  //   case SegmentedButtonsData.week:
-  //     final spots = <FlSpot>[];
-  //     for (final i in totalEx) {
-  //       // check if this week
-  //       if (i.expense!.createdDate!.isThisWeek) {
-  //         spots.add(
-  //           FlSpot(
-  //             i.expense!.createdDate!.toDoubleT,
-  //             i.expense?.ammount ?? 0,
-  //           ),
-  //         );
-  //       }
-  //     }
-  //     return spots;
-  //   case SegmentedButtonsData.month:
-  //     final spots = <FlSpot>[];
-  //     for (final i in totalEx) {
-  //       if (i.expense!.createdDate!.isThisMonth) {
-  //         spots.add(
-  //           FlSpot(
-  //             i.expense!.createdDate!.toDoubleT,
-  //             i.expense?.ammount ?? 0,
-  //           ),
-  //         );
-  //       }
-  //     }
-  //     return spots;
-  //   case SegmentedButtonsData.year:
-  //     final spots = <FlSpot>[];
-  //     for (final i in totalEx) {
-  //       if (i.expense!.createdDate!.isThisYear) {
-  //         spots.add(
-  //           FlSpot(
-  //             i.expense!.createdDate!.toDoubleT,
-  //             i.expense?.ammount ?? 0,
-  //           ),
-  //         );
-  //       }
-  //     }
-  //     return spots;
-  // }
 }
