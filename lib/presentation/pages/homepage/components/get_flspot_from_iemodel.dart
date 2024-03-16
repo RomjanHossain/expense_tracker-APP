214363e0a30fd1459432089e95f0a59284071899
RomjanHossain <romjan@skiff.com>
2024-03-16 12:50:13 +0600

    ie model

--- lib/presentation/pages/homepage/components/get_flspot_from_iemodel.dart
+++ lib/presentation/pages/homepage/components/get_flspot_from_iemodel.dart
@@ -8,9 +8,14 @@ import 'package:flutter/material.dart';
 double getMinAmountValueFromIEmodel(List<IEmodel> models) {
   final totalEx =
       models.where((element) => element.isIncome == ExpenseType.expense);
-  final min = totalEx
-      .map((e) => e.expense?.ammount ?? 0)
-      .reduce((value, element) => value < element ? value : element);
+  final amounts = totalEx.map((e) => e.expense?.ammount ?? 0);
+  if (amounts.isEmpty) {
+    return 0;
+  }
+  final min =
+      amounts.reduce((value, element) => value < element ? value : element);
+  // .reduce((value, element) => value < element ? value : element);
+  debugPrint('min amount: $min');
   return min;
 }
 
@@ -18,9 +23,14 @@ double getMinAmountValueFromIEmodel(List<IEmodel> models) {
 double getMaxAmountValueFromIEmodel(List<IEmodel> models) {
   final totalEx =
       models.where((element) => element.isIncome == ExpenseType.expense);
-  final max = totalEx
-      .map((e) => e.expense?.ammount ?? 0)
-      .reduce((value, element) => value > element ? value : element);
+  final amounts = totalEx.map((e) => e.expense?.ammount ?? 0);
+  // .reduce((value, element) => value > element ? value : element);
+  if (amounts.isEmpty) {
+    return 0;
+  }
+  final max =
+      amounts.reduce((value, element) => value > element ? value : element);
+  debugPrint('max amount: $max');
   return max;
 }
 
@@ -28,9 +38,17 @@ double getMaxAmountValueFromIEmodel(List<IEmodel> models) {
 double getMinCreatedDateValueFromIEmodel(List<IEmodel> models) {
   final totalEx =
       models.where((element) => element.isIncome == ExpenseType.expense);
-  final min = totalEx
-      .map((e) => e.expense!.createdDate!.toDoubleT)
-      .reduce((value, element) => value < element ? value : element);
+  final createdDates = totalEx.map((e) => e.expense!.createdDate!.toDoubleT);
+  if (createdDates.isEmpty) {
+    return 0;
+  }
+  final min = createdDates.reduce(
+    (value, element) {
+      return value < element ? value : element;
+    },
+  );
+
+  debugPrint('min createdDate: $min');
   return min;
 }
 
@@ -38,9 +56,14 @@ double getMinCreatedDateValueFromIEmodel(List<IEmodel> models) {
 double getMaxCreatedDateValueFromIEmodel(List<IEmodel> models) {
   final totalEx =
       models.where((element) => element.isIncome == ExpenseType.expense);
-  final max = totalEx
-      .map((e) => e.expense!.createdDate!.toDoubleT)
+  final createdDates = totalEx.map((e) => e.expense!.createdDate!.toDoubleT);
+  if (createdDates.isEmpty) {
+    return 0;
+  }
+  final max = createdDates
       .reduce((value, element) => value > element ? value : element);
+  // .reduce((value, element) => value > element ? value : element);
+  debugPrint('max createdDate: $max');
   return max;
 }
 
@@ -53,20 +76,33 @@ List<FlSpot> getFlSpotFromIEmodelToday(
       models.where((element) => element.isIncome == ExpenseType.expense);
 
   final spots = <FlSpot>[];
+  final c_s = totalEx.map((e) => e.expense!.createdDate!);
+  final x_s = totalEx.map((e) => e.expense!.createdDate!.toDoubleT);
+  final y_s = totalEx.map((e) => e.expense?.ammount ?? 0);
+  debugPrint('c_s: $c_s');
+  debugPrint('x_s: $x_s');
+  return List.generate(
+    x_s.length,
+    (index) => FlSpot(
+      x_s.elementAt(index),
+      y_s.elementAt(index),
+    ),
+  );
   // debugPrint('Total Len: ${totalEx.length}');
-  for (final i in totalEx) {
-    final x = i.expense!.createdDate!.toDoubleT;
-    final y = i.expense?.ammount ?? 0;
-    debugPrint(
-      'x: $x, y = $y',
-    );
-    spots.add(
-      FlSpot(
-        x,
-        y,
-      ),
-    );
-  }
+  // for (final i in totalEx) {
+  //   final x = i.expense!.createdDate!.toDoubleT;
+  //   final y = i.expense?.ammount ?? 0;
+  //   debugPrint(
+  //     'x: $x, y = $y',
+  //   );
+  //   spots.add(
+  //     FlSpot(
+  //       x,
+  //       y,
+  //     ),
+  //   );
+  // }
+  debugPrint('Len of sports: ${spots.length}');
   return spots;
 }
 
