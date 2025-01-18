0d23953b89764bccc45626ec9f81e554a36c3e77
RomjanHossain <romjan1412@proton.me>
2025-01-18 17:02:38 +0600

    splash

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,7 +1,12 @@
 import 'package:expense_tracker/app/app.dart';
 import 'package:expense_tracker/bootstrap.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-void main() async{
-    await ScreenUtil.ensureScreenSize();
+
+void main() async {
+  final widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  await ScreenUtil.ensureScreenSize();
   await bootstrap(() => const App());
 }
